23aab8a69efe0067d02055d575691a72ca1668bd
imgwang-yeol <mkitigy@gmail.com>
2021-08-24 19:49:21 +0900

    chores: compression-webpack-plugin 설치

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
@@ -6,6 +7,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [new CompressionPlugin()],
   optimization: {
     minimizer: ['...', new CssMinimizerPlugin()],
   },
