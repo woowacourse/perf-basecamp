962a7eee3325ce4a854a85bab703cd2f94c78140
bigsaigon333 <likepeppermint@gmail.com>
2021-08-24 17:11:49 +0900

    chore: stylelint의 code formatting 기능을 제외시킨다

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-hudochenkov/order",
+    "stylelint-config-prettier",
+  ],
+  plugins: ["stylelint-order"],
+};
