a24cdcb58a4884acb97b1eb4ea46351d38413868
imgwang-yeol <mkitigy@gmail.com>
2021-08-24 15:15:18 +0900

    chores: webpack 설정 파일 분리

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './public', to: './public' }],
+    }),
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'static/[name].[ext]',
+        },
+      },
+    ],
+  },
+};
