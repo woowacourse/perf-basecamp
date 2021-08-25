c4d2024e060a9d3dd93dfae4aedf8e8448b8dcf3
0imbean0 <tjsqls2003@gmail.com>
2021-08-25 22:41:13 +0900

    feat: css extract 및 minify
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -25,6 +27,7 @@ module.exports = {
       patterns: [{ from: "./public", to: "./public" }],
     }),
     new Dotenv(),
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
@@ -37,7 +40,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -49,6 +52,7 @@ module.exports = {
     ],
   },
   optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
     minimize: false,
   },
 };
