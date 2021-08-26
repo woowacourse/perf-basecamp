d9147fb53334a1bc621546908889953b9362adba
zigsong <wldms5764@gmail.com>
2021-08-26 14:35:26 +0900

    chore: yarn install & fix indent

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,29 +12,29 @@ import findGif from "../../assets/images/find.gif";
 import styles from "./Home.module.css";
 
 const Home = () => {
-    return (
-        <>
-            <NavBar />
-            <section className={styles.heroSection}>
-                <img className={styles.heroImage} src={heroImage} alt="hero" />
-                <div className={styles.projectTitle}>
-                    <h1 className={styles.title}>Memegle</h1>
-                    <h3 className={styles.subtitle}>gif search engine for you</h3>
-                </div>
-                <Link to="/search"><button className={styles.cta}>start search</button></Link>
-            </section>
-            <section className={styles.featureSection}>
-                <div className={styles.featureSectionWrapper}>
-                    <h2 className={styles.featureTitle}>Features</h2>
-                    <div className={styles.featureItemContainer}>
-                        <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-                        <FeatureItem title="Find gif for free" imageSrc={findGif} />
-                    </div>
-                </div>
-            </section>
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<NavBar />
+			<section className={styles.heroSection}>
+				<img className={styles.heroImage} src={heroImage} alt="hero" />
+				<div className={styles.projectTitle}>
+					<h1 className={styles.title}>Memegle</h1>
+					<h3 className={styles.subtitle}>gif search engine for you</h3>
+				</div>
+				<Link to="/search"><button className={styles.cta}>start search</button></Link>
+			</section>
+			<section className={styles.featureSection}>
+				<div className={styles.featureSectionWrapper}>
+					<h2 className={styles.featureTitle}>Features</h2>
+					<div className={styles.featureItemContainer}>
+						<FeatureItem title="See trending gif" imageSrc={trendingGif} />
+						<FeatureItem title="Find gif for free" imageSrc={findGif} />
+					</div>
+				</div>
+			</section>
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
