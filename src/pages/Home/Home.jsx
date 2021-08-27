ceacdce423b6c5eef9a4d26fa00bba2342a764a2
Tanney <dhxodms89@gmail.com>
2021-08-27 23:35:56 +0900

    feat: 이미지 사이즈 최적화
    - 이미지 사이즈, 용량, 메타데이터 최적화
    - srcset 적용

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,8 @@ import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import FeatureItem from "../../components/FeatureItem/FeatureItem";
 
-import heroImage from "../../assets/images/hero.png"
+import heroImage_1024 from "../../assets/images/hero-1024.jpeg"
+import heroImage_1920 from "../../assets/images/hero-1920.jpeg"
 import trendingGif from "../../assets/images/trending.gif";
 import findGif from "../../assets/images/find.gif";
 
@@ -16,7 +17,7 @@ const Home = () => {
         <>
             <NavBar />
             <section className={styles.heroSection}>
-                <img className={styles.heroImage} src={heroImage} alt="hero" />
+                <img className={styles.heroImage} srcSet={`${heroImage_1024} 1024w, ${heroImage_1920} 1920w`} src={heroImage_1920} alt="hero" />
                 <div className={styles.projectTitle}>
                     <h1 className={styles.title}>Memegle</h1>
                     <h3 className={styles.subtitle}>gif search engine for you</h3>
--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif)$/i,
         loader: "file-loader",
         options: {
           name: "static/[name].[ext]"
