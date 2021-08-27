c6c88909df5640a2d1ab4ac28f3b964744170001
seojihwan <seojihwan213@naver.com>
2021-08-28 00:17:48 +0900

    refactor: 푸터 헤더 App으로 이동

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Suspense } from "react";
+import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Home = React.lazy(() => import("./pages/Home/Home"));
 const Search = React.lazy(() => import("./pages/Search/Search"));
 
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Suspense fallback={null}>
-            <Home />
-          </Suspense>
-        </Route>
-        <Route exact path="/search">
-          <Suspense fallback={null}>
-            <Search />
-          </Suspense>
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Suspense fallback={null}>
+              <Home />
+            </Suspense>
+          </Route>
+          <Route exact path="/search">
+            <Suspense fallback={null}>
+              <Search />
+            </Suspense>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 };
 
