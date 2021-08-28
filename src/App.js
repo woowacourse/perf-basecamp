60b5fe8c9b149583cbff8095e252310c434925c4
zereight <fullstackmachinelearning@gmail.com>
2021-08-28 13:41:51 +0900

    fix: Home 페이지에서 불러오는 스크립트 리소스에 gif 검색을 위한 giphy 모듈이 포함되어 있지 않도록 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-import Search from "./pages/Search/Search";
 
 import "./App.css";
 
+const Home = lazy(() => import("./pages/Home/Home"));
+const Search = lazy(() => import("./pages/Search/Search"));
+
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/search" component={Search} />
-      </Switch>
-    </Router>
+    <Suspense fallback={<>{"로딩중입니다."}</>}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+        </Switch>
+      </Router>
+    </Suspense>
   );
-}
+};
 
 export default App;
