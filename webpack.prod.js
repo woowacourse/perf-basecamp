86c9e1183f72684b53202b78f9669580c1edba58
shinsehantan <shinsehantan@users.noreply.github.com>
2021-08-29 00:28:52 +0900

    chore: 압축 알고리즘 gzip에서 brotli로 변경
    - gfx/zopfli 라이브러리 설치
    - CompressionPlugin 설정 수정

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,23 @@ const common = require("./webpack.common.js");
 const CompressionPlugin = require("compression-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const zlib = require("zlib");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [
     new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
       threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
     }),
   ],
   optimization: {
