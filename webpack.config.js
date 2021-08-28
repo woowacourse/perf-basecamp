599d435738e3ab84ba134cb2b0ec9c6e01b9a637
yujo <yujo.dev@gmail.com>
2021-08-28 16:39:47 +0900

    chore: env에 따른 source-map 옵션 추가 및 minimize 옵션 활성화
    - .js 번들 사이즈 750 -> 240

--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,53 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  resolve: { extensions: [".js", ".jsx"] },
-  output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/dist"),
-    clean: true
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
-  devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "./public", to: "./public" }]
-    }),
-    new Dotenv()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: "file-loader",
-        options: {
-          name: "static/[name].[ext]"
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: false
-  }
+module.exports = () => {
+  const isDevelopment = process.env.NODE_ENV !== "production";
+
+  return {
+    mode: isDevelopment ? "development" : "production",
+    devtool: isDevelopment ? "eval-source-map" : "source-map",
+    entry: "./src/index.js",
+    resolve: { extensions: [".js", ".jsx"] },
+    output: {
+      filename: "bundle.js",
+      assetModuleFilename: "images/[hash][ext][query]",
+      path: path.join(__dirname, "/dist"),
+      clean: true,
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: "./public", to: "./public" }],
+      }),
+      new Dotenv(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
 };
