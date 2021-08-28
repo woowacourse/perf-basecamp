d64e6e15d9df4ca538d0391b10c9062d024dc228
Chayan <hchayan196@gmail.com>
2021-08-28 15:37:43 +0900

    refactor: 검색한요소 재랜더링 방지 React.memo 적용

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./GifItem.module.css";
+import styles from './GifItem.module.css';
 
-const GifItem = ({ imageUrl = "", title = "" }) => {
-    return (
-        <div className={styles.gifItem}>
-            <img className={styles.gifImage} src={imageUrl} />
-            <div className={styles.gifTitleContainer}>
-                <div className={styles.gifTitleBg}></div>
-                <h4 className={styles.gifTitle}>{title}</h4>
-            </div>
-        </div>
-    );
-}
+const GifItem = React.memo(({ imageUrl = '', title = '' }) => {
+  return (
+    <div className={styles.gifItem}>
+      <img className={styles.gifImage} src={imageUrl} />
+      <div className={styles.gifTitleContainer}>
+        <div className={styles.gifTitleBg}></div>
+        <h4 className={styles.gifTitle}>{title}</h4>
+      </div>
+    </div>
+  );
+});
 
 export default GifItem;
