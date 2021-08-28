1957ad2934ffb10d9f49507e6bc5d9b9389d4eb0
shinsehantan <shinsehantan@users.noreply.github.com>
2021-08-29 00:17:09 +0900

    chore: minify, gzip 적용하여 소스코드 압축
    - css minify를 위해 css-minimizer-webpack-plugin 설치 및 적용
    - js minify를 위해 terser-webpack-plugin 적용
    - gzip 압축을 위해 compression-webpack-plugin 설치 및 적용

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CompressionPlugin = require("compression-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new CompressionPlugin({
+      threshold: 10240,
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: ["...", new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 });
