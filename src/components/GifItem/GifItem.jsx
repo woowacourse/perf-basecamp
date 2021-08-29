a047359bc9159c44df3946ef5bc5253cd87cce9c
Chayan <hchayan196@gmail.com>
2021-08-29 18:57:49 +0900

    refactor: 불러오는 gif alt 속성 추가

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -5,7 +5,7 @@ import styles from './GifItem.module.css';
 const GifItem = React.memo(({ imageUrl = '', title = '' }) => {
   return (
     <div className={styles.gifItem}>
-      <img className={styles.gifImage} src={imageUrl} />
+      <img className={styles.gifImage} src={imageUrl} alt={title} />
       <div className={styles.gifTitleContainer}>
         <div className={styles.gifTitleBg}></div>
         <h4 className={styles.gifTitle}>{title}</h4>
