2118a5e9b8993d0362b47d488c2f39f59151064f
iborymagic <mlch8023@gmail.com>
2021-08-29 22:59:50 +0900

    chore: 소스코드 크기 줄이기 적용 - production 모드에서 optimization을 true로 변경 - MiniCssExtractPlugin 적용 - CssMinimizerPlugin 적용 - HtmlWebpackPlugin에 CollapseWhitespace, removeComments 적용 - CleanWebpackPlugin 적용(빌드 디렉토리 청소)

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,91 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
-  resolve: { extensions: [".js", ".jsx"] },
-  output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/dist"),
-    clean: true
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
-  devtool: "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "./public", to: "./public" }]
-    }),
-    new Dotenv()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: "file-loader",
-        options: {
-          name: "static/[name].[ext]"
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: false
+module.exports = (env) => {
+  const mode = process.env.NODE_ENV || 'development';
+
+  const config = {
+    entry: './src/index.js',
+    resolve: { extensions: ['.js', '.jsx'] },
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, '/dist'),
+      clean: true,
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+    devtool: mode === 'development' ? 'inline-source-map' : false,
+    plugins: [
+      new HtmlWebpackPlugin({
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+        template: './index.html',
+        hash: true,
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './public', to: './public' }],
+      }),
+      new Dotenv(),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: 'bundle-report.html',
+        openAnalyzer: false,
+      }),
+      new CleanWebpackPlugin(),
+      new webpack.DefinePlugin({}),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'static/[name].[ext]',
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimize: mode === 'development' ? false : true,
+      minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+  };
+
+  if (mode === 'production') {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: `[name].css`,
+      })
+    );
   }
+
+  config.module.rules.push({
+    test: /\.css$/i,
+    exclude: /node_modules/,
+    use: [
+      mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+      'css-loader',
+    ],
+  });
+
+  return config;
 };
