b1cd398ae38ab23c31e0bef0b8896077620a8040
Min Kyung Kang <mnk918p@gmail.com>
2021-08-31 02:13:41 +0900

    fix: 접근성을 위한 image alt 추가

--- src/components/FeatureItem/FeatureItem.jsx
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-
-import styles from "./FeatureItem.module.css";
+import React from 'react';
+import styles from './FeatureItem.module.css';
 
 const FeatureItem = ({ title, imageSrc }) => {
-    return (
-        <div className={styles.featureItem}>
-            <img className={styles.featureImage} src={imageSrc} />
-            <div className={styles.featureTitleBg}></div>
-            <h4 className={styles.featureTitle}>{title}</h4>
-        </div>
-    );
-}
+  return (
+    <div className={styles.featureItem}>
+      <img className={styles.featureImage} src={imageSrc} alt='featureImage' />
+      <div className={styles.featureTitleBg}></div>
+      <h4 className={styles.featureTitle}>{title}</h4>
+    </div>
+  );
+};
 
 export default FeatureItem;
