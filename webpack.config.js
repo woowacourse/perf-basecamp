e489f022fa31bd98499153dca5b92d8d7d313f15
yungo1846 <yungo1468@naver.com>
2021-08-30 12:02:24 +0900

    chore: remove contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,12 @@ const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const { DefinePlugin } = require("webpack");
-const package = require("./package.json");
 
 module.exports = {
   entry: "./src/index.js",
   resolve: { extensions: [".js", ".jsx"] },
   output: {
-    filename: `v${package.version}.[name].[contenthash].js`,
+    filename: `bundle.js`,
     path: path.join(__dirname, "/dist"),
     clean: true,
   },
