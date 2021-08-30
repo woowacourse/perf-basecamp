24590d02a004e416352c6c3ea480f91f8928d41c
yujo <yujo.dev@gmail.com>
2021-08-30 22:07:18 +0900

    refactor: 불필요한 파일 제거 및 video preload 설정

--- src/components/FeatureItem/FeatureItem.jsx
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -5,7 +5,14 @@ import styles from "./FeatureItem.module.css";
 const FeatureItem = ({ title, videoSrc }) => {
   return (
     <div className={styles.featureItem}>
-      <video className={styles.featureImage} autoPlay loop muted playsInline>
+      <video
+        className={styles.featureImage}
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload
+      >
         <source src={videoSrc} type="video/mp4" />
       </video>
       <div className={styles.featureTitleBg}></div>
