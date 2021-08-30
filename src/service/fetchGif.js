eb38b732d863099b941554122e1e9b6010d56109
yujo <yujo.dev@gmail.com>
2021-08-31 08:15:19 +0900

    refactor: GIPHY의 trending API를 새로 요청하지 않도록 변경

--- src/service/fetchGif.js
+++ src/service/fetchGif.js
@@ -1,5 +1,18 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
 
+const TRENDING_GIFS = "TRENDING_GIFS";
+const KEYWORD_GIFS = (keyword) => `${keyword.toUpperCase()}_GIFS`;
+
+const local = {
+  get: (key) => {
+    return JSON.parse(localStorage.getItem(key)) ?? [];
+  },
+
+  set: (key, item) => {
+    localStorage.setItem(key, JSON.stringify(item));
+  },
+};
+
 /**
  * 응답 예제는 아래 링크에서 참고
  * https://developers.giphy.com/explorer/#explorer
@@ -18,24 +31,44 @@ const formatResponse = (gifList) => {
   });
 };
 
-export const fetchTrendingGifs = () => {
-  return fetch(TRENDING_GIF_API)
+export const fetchTrendingGifs = async () => {
+  const localTrendingGifs = local.get(TRENDING_GIFS);
+
+  if (localTrendingGifs.length) {
+    return localTrendingGifs;
+  }
+
+  const results = await fetch(TRENDING_GIF_API)
     .then((response) => response.json())
     .then((gifs) => gifs.data)
     .then(formatResponse)
     .catch((e) => {
       return [];
     });
+
+  local.set(TRENDING_GIFS, results);
+
+  return results;
 };
 
-export const fetchGifsByKeyword = (keyword, page = 0) => {
+export const fetchGifsByKeyword = async (keyword, page = 0) => {
+  const localKeywordGifs = local.get(KEYWORD_GIFS(keyword));
+
+  if (localKeywordGifs.length) {
+    return localKeywordGifs;
+  }
+
   const offset = page * DEFAULT_FETCH_COUNT;
 
-  return gf
+  const results = await gf
     .search(keyword, { limit: DEFAULT_FETCH_COUNT, lang: "en", offset })
     .then((gifs) => gifs.data)
     .then(formatResponse)
     .catch((e) => {
       return [];
     });
+
+  local.set(KEYWORD_GIFS(keyword), results);
+
+  return results;
 };
