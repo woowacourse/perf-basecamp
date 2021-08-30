da19ac5a796fa426d2df9effa701ea349b6fefe1
zigsong <wldms5764@gmail.com>
2021-08-30 18:08:13 +0900

    refactor: add fallback option in loadable component

--- src/pages/Loadable/Loadable.jsx
+++ src/pages/Loadable/Loadable.jsx
@@ -1,5 +1,7 @@
 import loadable from '@loadable/component'
 
-const SearchLoadable = loadable(() => import('../Search/Search'))
+const SearchLoadable = loadable(() => import('../Search/Search'), {
+  fallback: <div>Loading...</div>
+})
 
 export { SearchLoadable };
