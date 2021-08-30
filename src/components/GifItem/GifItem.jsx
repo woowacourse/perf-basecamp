7cc704992e156c210fe2fd0aea8e750cf086a855
yungo1846 <yungo1468@naver.com>
2021-08-30 12:00:22 +0900

    feat: adopt memo to GifItem

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import styles from "./GifItem.module.css";
 
 const GifItem = ({ imageUrl = "", title = "" }) => {
-    return (
-        <div className={styles.gifItem}>
-            <img className={styles.gifImage} src={imageUrl} />
-            <div className={styles.gifTitleContainer}>
-                <div className={styles.gifTitleBg}></div>
-                <h4 className={styles.gifTitle}>{title}</h4>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.gifItem}>
+      <img className={styles.gifImage} src={imageUrl} />
+      <div className={styles.gifTitleContainer}>
+        <div className={styles.gifTitleBg}></div>
+        <h4 className={styles.gifTitle}>{title}</h4>
+      </div>
+    </div>
+  );
+};
 
-export default GifItem;
+export default React.memo(GifItem);
