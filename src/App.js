ec974fc115ffe34629dbb7d6715851c12ce68d58
yujo <yujo.dev@gmail.com>
2021-08-30 21:07:01 +0900

    refactor: 코드 스플리팅 적용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-import Search from "./pages/Search/Search";
 
 import "./App.css";
 
-const App = () => {
-  return (
-    <Router>
+const Home = lazy(() => import("./pages/Home/Home"));
+const Search = lazy(() => import("./pages/Search/Search"));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/search" component={Search} />
       </Switch>
-    </Router>
-  );
-};
+    </Suspense>
+  </Router>
+);
 
 export default App;
