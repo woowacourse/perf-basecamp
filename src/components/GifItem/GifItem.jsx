5150cacc38a507491106f578f43ad34beb49d925
권세진 <0307kwon@gmail.com>
2021-08-30 11:00:44 +0900

    refactor: 새로운 요소만 렌더링하도록 수정

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import styles from "./GifItem.module.css";
 
-const GifItem = ({ imageUrl = "", title = "" }) => {
-    return (
-        <div className={styles.gifItem}>
-            <img className={styles.gifImage} src={imageUrl} />
-            <div className={styles.gifTitleContainer}>
-                <div className={styles.gifTitleBg}></div>
-                <h4 className={styles.gifTitle}>{title}</h4>
-            </div>
-        </div>
-    );
-}
+const GifItem = React.memo(function GifItem({ imageUrl = "", title = "" }) {
+  return (
+    <div className={styles.gifItem}>
+      <img className={styles.gifImage} src={imageUrl} />
+      <div className={styles.gifTitleContainer}>
+        <div className={styles.gifTitleBg}></div>
+        <h4 className={styles.gifTitle}>{title}</h4>
+      </div>
+    </div>
+  );
+});
 
 export default GifItem;
