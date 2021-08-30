0e7e6baf4584ba85ec105d9f2eccc1dec9d1aee4
0imbean0 <tjsqls2003@gmail.com>
2021-08-30 18:43:35 +0900

    feat: trendingGif cache 적용
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.css";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const Search = lazy(() => import("./pages/Search/Search"));
 
-import "./App.css";
-
 const App = () => {
+  const [trendingGifs, setTrendingGifs] = useState(null);
+
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/search">
+            <Search
+              trendingGifs={trendingGifs}
+              setTrendingGifs={setTrendingGifs}
+            />
+          </Route>
         </Switch>
       </Suspense>
     </Router>
