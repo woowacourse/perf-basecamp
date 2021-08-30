7ff25a84b4c02c001c88ac3ab9f9da1c2bcd53cc
Shim MunSeong <Puterism.K@gmail.com>
2021-08-30 20:08:53 +0900

    refactor: GIPHY의 trending API를 Search 페이지에 들어올 때마다 새로 요청하지 않도록 수정
    - 요청 API 함수를 memoize 처리

--- src/service/fetchGif.js
+++ src/service/fetchGif.js
@@ -8,6 +8,17 @@ const gf = new GiphyFetch(process.env.GIPHY_API_KEY);
 const DEFAULT_FETCH_COUNT = 16;
 const TRENDING_GIF_API = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=${DEFAULT_FETCH_COUNT}&rating=g`;
 
+const memoizePromise = (() => {
+  const cache = new Map();
+
+  return (key, promiseFn) => async () => {
+    if (cache.has(key)) return cache.get(key);
+
+    cache.set(key, promiseFn);
+    return cache.get(key);
+  };
+})();
+
 const formatResponse = (gifList) => {
   return gifList.map((gif) => {
     return {
@@ -18,15 +29,16 @@ const formatResponse = (gifList) => {
   });
 };
 
-export const fetchTrendingGifs = () => {
-  return fetch(TRENDING_GIF_API)
+export const fetchTrendingGifs = memoizePromise(
+  'TRENDING_GIFS',
+  fetch(TRENDING_GIF_API)
     .then((response) => response.json())
     .then((gifs) => gifs.data)
     .then(formatResponse)
     .catch((e) => {
       return [];
-    });
-};
+    })
+);
 
 export const fetchGifsByKeyword = (keyword, page = 0) => {
   const offset = page * DEFAULT_FETCH_COUNT;
