b34bffe6da480b3c7748445e040bf91f9c181950
Min Kyung Kang <mnk918p@gmail.com>
2021-08-30 15:37:51 +0900

    chore: webpack bundle analyzer 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./src/index.js",
-  resolve: { extensions: [".js", ".jsx"] },
+  entry: './src/index.js',
+  resolve: { extensions: ['.js', '.jsx'] },
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/dist"),
-    clean: true
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+    clean: true,
   },
   devServer: {
     hot: true,
     open: true,
     historyApiFallback: true,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      minify: {
+        collapseWhiteSpace: true,
+        removeComments: true,
+      },
+      hash: true,
+      template: './index.html',
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./public", to: "./public" }]
+      patterns: [{ from: './public', to: './public' }],
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   module: {
     rules: [
@@ -32,26 +39,23 @@ module.exports = {
         test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "static/[name].[ext]"
-        }
-      }
-    ]
+          name: 'static/[name].[ext]',
+        },
+      },
+    ],
   },
   optimization: {
-    minimize: false
-  }
+    minimize: false,
+  },
 };
