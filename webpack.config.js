86fc8b689dccb0bb11ad7351a4b08825679c5a67
swon3210 <swon3210@gmail.com>
2021-08-31 15:25:45 +0900

    chore: 웹팩 설정
    - 소스코드 최적화 및 난독화
    - css 를 자바스크립트가 아닌 별도의 css 파일로 추출
    - url-loader 에서 10kb 미만의 파일은 inline 으로 내장되도록 설정
    - chunk 파일은 id 를, bundle 파일은 이름을 바탕으로 번들링 결과물의 이름을 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,102 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCSSExtractionPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const chunkFileNames = ['service_fetchGif', 'page_Search'];
+
+const DEVELOPMENT = 'development';
+
+const mode = process.env.NODE_ENV || DEVELOPMENT;
 
 module.exports = {
-  entry: "./src/index.js",
-  resolve: { extensions: [".js", ".jsx"] },
+  mode,
+  entry: './src/index.js',
+  resolve: { extensions: ['.js', '.jsx'] },
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/dist"),
-    clean: true
+    filename: '[name].bundle.js',
+    chunkFilename: (pathData) => {
+      return chunkFileNames.includes(pathData.chunk.name)
+        ? '[name].js'
+        : '[id].js';
+    },
+    path: path.join(__dirname, '/dist'),
+    clean: true,
   },
   devServer: {
     hot: true,
     open: true,
     historyApiFallback: true,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
+    new BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      minify: {
+        collapseWhitespace: true, // 빈칸 제거
+        removeComments: true, // 주석 제거
+      },
+      template: './index.html',
+      hash: true, // 결과물 index.html 이 매 빌드마다 다른 queryString과 함께 bundle.js 를 불러들이도록 함으로써 파일이 동일하지 않다는 것을 알림 - 캐싱 방지
+    }),
+    new MiniCSSExtractionPlugin({
+      filename: '[name].css',
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./public", to: "./public" }]
+      patterns: [
+        { from: './public', to: './public' },
+        {
+          from: './src/assets/font',
+          to: './font',
+        },
+      ],
     }),
-    new Dotenv()
+    new Dotenv(),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: [MiniCSSExtractionPlugin.loader, 'css-loader'],
+      },
+      {
         test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        test: /\.(png|jpg|jpeg|gif|webp)$/i,
+        loader: 'image-webpack-loader',
+        enforce: 'pre',
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: "file-loader",
+        test: /\.(svg|ttf|png|jpg|gif|webp)$/i,
+        loader: 'url-loader',
         options: {
-          name: "static/[name].[ext]"
-        }
-      }
-    ]
+          name: 'static/[name].[ext]',
+          limit: 10 * 1024,
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          name: 'static/[name].[ext]',
+          limit: 10 * 1024,
+          noquotes: true,
+        },
+      },
+    ],
   },
   optimization: {
-    minimize: false
-  }
+    minimizer: [new OptimizeCSSAssetsPlugin(), new TerserPlugin()],
+  },
 };
