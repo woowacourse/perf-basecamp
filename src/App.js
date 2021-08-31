d446038e999ffcf60f98416085c3f4b06ac2fb1e
sunhpark42 <sunhpark42@gmail.com>
2021-08-31 19:53:18 +0900

    feat: react.lazy를 이용한 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from "./pages/Home/Home";
-import Search from "./pages/Search/Search";
+import Home from './pages/Home/Home';
 
-import "./App.css";
+import './App.css';
+
+const SearchPage = React.lazy(() => import('./pages/Search/Search'));
+
+const SearchPageSuspense = () => {
+  return (
+    <Suspense fallback={<div>로딩중...</div>}>
+      <SearchPage />
+    </Suspense>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/search" component={Search} />
+        <Route exact path="/search" component={SearchPageSuspense} />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
