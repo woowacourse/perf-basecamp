8b4c371251acd23b1a40842a7e8ad8689e284187
iborymagic <mlch8023@gmail.com>
2021-08-31 17:06:44 +0900

    fix: Home 페이지의 h2, h3 태그 순서 변경

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
-          <h3 className={styles.subtitle}>gif search engine for you</h3>
+          <h2 className={styles.subtitle}>gif search engine for you</h2>
         </div>
         <Link to="/search">
           <button className={styles.cta}>start search</button>
@@ -31,7 +31,7 @@ const Home = () => {
       </section>
       <section className={styles.featureSection}>
         <div className={styles.featureSectionWrapper}>
-          <h2 className={styles.featureTitle}>Features</h2>
+          <h3 className={styles.featureTitle}>Features</h3>
           <div className={styles.featureItemContainer}>
             <FeatureItem
               title="See trending gif"
