d549c4d0171e9e0c272cf9235cfb8118ad10a1b8
imgwang-yeol <mkitigy@gmail.com>
2021-09-01 08:58:46 +0900

    style: 미사용 import 제거, 변수명 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,8 @@ import Footer from '../../components/Footer/Footer';
 import FeatureItem from '../../components/FeatureItem/FeatureItem';
 
 import heroImage from '../../assets/images/hero.webp';
-import trendingGif from '../../assets/images/trending.mp4';
-import findGif from '../../assets/images/find.mp4';
+import trendingVideo from '../../assets/images/trending.mp4';
+import findVideo from '../../assets/images/find.mp4';
 
 import styles from './Home.module.css';
 
@@ -29,8 +29,8 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
+            <FeatureItem title="See trending gif" imageSrc={trendingVideo} />
+            <FeatureItem title="Find gif for free" imageSrc={findVideo} />
           </div>
         </div>
       </section>
