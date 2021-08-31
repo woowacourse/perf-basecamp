928e5efc969af495d98e88e2633c2525daf2e962
sunhpark42 <sunhpark42@gmail.com>
2021-08-31 21:57:23 +0900

    feat: trendingGifs를 최초 로드시에만 fetching

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 
 import './App.css';
+import { fetchTrendingGifs } from './service/fetchGif';
 
 const SearchPage = React.lazy(() => import('./pages/Search/Search'));
 
-const SearchPageSuspense = () => {
-  return (
-    <Suspense fallback={<div>로딩중...</div>}>
-      <SearchPage />
-    </Suspense>
-  );
-};
+const SearchPageSuspense =
+  ({ trendingGifs }) =>
+  () => {
+    return (
+      <Suspense fallback={<div>로딩중...</div>}>
+        <SearchPage trendingGifs={trendingGifs} />
+      </Suspense>
+    );
+  };
 
 const App = () => {
+  const [trendingGifs, setTrendingGifs] = useState([]);
+
+  useEffect(async () => {
+    const gifs = await fetchTrendingGifs();
+
+    setTrendingGifs(gifs);
+  }, []);
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/search" component={SearchPageSuspense} />
+        <Route exact path="/" component={Home} />9
+        <Route exact path="/search" component={SearchPageSuspense({ trendingGifs })} />
       </Switch>
     </Router>
   );
