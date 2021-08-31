58acd19847a6f5eb7a30f79113930b15ffca7a61
2sooy <1401660@naver.com>
2021-08-31 10:47:56 +0900

    feat: trending gif 캐싱을 통해 api call 요청하지 않기

--- src/service/fetchGif.js
+++ src/service/fetchGif.js
@@ -1,4 +1,5 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
+import { memorize } from "../util/memorize";
 
 /**
  * 응답 예제는 아래 링크에서 참고
@@ -9,32 +10,35 @@ const DEFAULT_FETCH_COUNT = 16;
 const TRENDING_GIF_API = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=${DEFAULT_FETCH_COUNT}&rating=g`;
 
 const formatResponse = (gifList) => {
-    return gifList.map(gif => {
-        return {
-            id: gif.id,
-            title: gif.title,
-            imageUrl: gif.images.original.url
-        }
-    });
-}
+  return gifList.map((gif) => {
+    return {
+      id: gif.id,
+      title: gif.title,
+      imageUrl: gif.images.original.url,
+    };
+  });
+};
 
-export const fetchTrendingGifs = () => {
-    return fetch(TRENDING_GIF_API)
-        .then(response => response.json())
-        .then(gifs => gifs.data)
-        .then(formatResponse)
-        .catch(e => {
-            return [];
-        });
-}
+const fetchTrendingGifs = async () => {
+  return await fetch(TRENDING_GIF_API)
+    .then((response) => response.json())
+    .then((gifs) => gifs.data)
+    .then(formatResponse)
+    .catch((e) => {
+      return [];
+    });
+};
+
+export const fetchTrendingGifsMemo = memorize(fetchTrendingGifs);
 
 export const fetchGifsByKeyword = (keyword, page = 0) => {
-    const offset = page * DEFAULT_FETCH_COUNT;
+  const offset = page * DEFAULT_FETCH_COUNT;
 
-    return gf.search(keyword, { limit: DEFAULT_FETCH_COUNT, lang: "en", offset })
-        .then(gifs => gifs.data)
-        .then(formatResponse)
-        .catch(e => {
-            return [];
-        });
-}
+  return gf
+    .search(keyword, { limit: DEFAULT_FETCH_COUNT, lang: "en", offset })
+    .then((gifs) => gifs.data)
+    .then(formatResponse)
+    .catch((e) => {
+      return [];
+    });
+};
