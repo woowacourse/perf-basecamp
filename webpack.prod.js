eae861c9555f98ff93f24da0dad05b4728fee645
imgwang-yeol <mkitigy@gmail.com>
2021-09-01 09:49:12 +0900

    chores: 필요없는 plugin 삭제

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,7 @@
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  optimization: {
-    minimizer: ['...', new CssMinimizerPlugin()],
-  },
 });
