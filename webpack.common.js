4b701d7c784ba8b039b27f6d71e5149b0c73f477
imgwang-yeol <mkitigy@gmail.com>
2021-09-01 09:28:30 +0900

    chores: file-loader -> asset modules

--- webpack.common.js
+++ webpack.common.js
@@ -28,10 +28,7 @@ module.exports = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|webp|gif|mp4)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]',
-        },
+        type: 'asset/resource',
       },
     ],
   },
