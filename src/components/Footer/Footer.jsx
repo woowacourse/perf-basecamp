6ee25da9f0b679caf097d1d880249a272cb8601c
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-09-01 22:35:55 +0900

    [성능 베이스캠프] 디토(천영석) 미션 제출합니다. (#8)
    * chore: .env 추가 및 npm install
    
    * etc: optimization 삭제
    
    - 기본 값이 minimize: true이기 때문
    
    * etc: production이 아닐 때 source-map 삭제
    
    * etc: jpg, gif 리사이즈 및 압축
    
    * etc: 코드 스플리팅 설정
    
    * etc: 사용하지 않는 useRef 삭제
    
    * feat: 이미 트랜딩 데이터를 불러온 후에는 재 요청x
    
    * etc: hover 이벤트 시 top -> translateY로 교체
    
    * refactor: 리렌더링 되지 않아도 되는 컴포넌트 memo로 변경
    
    * refactor: gif -> mp4로 변경
    
    * etc: file-loader -> asset/resource 변경
    
    * refactor: trending 요청 함수 자체에서 캐싱하도록 변경
    
    * refactor: mp4 -> webm으로 변경
    
    * etc: mode -> node-env로 변경
    
    * refactor: 불필요한 memo 삭제
    
    * refactor: url gif -> mp4로 변경
    
    * refactor: layoutshift 문제 해결
    
    * etc: bundle.js에 chunkhash 적용

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { memo } from 'react';
 
-import "./Footer.module.css";
+import './Footer.module.css';
 
-const Footer = () => {
-    return (
-        <footer>
-            <p>memegle 2021. All rights reserved. Powered by <a href="https://giphy.com/">Giphy</a>.</p>
-        </footer>
-    );
-}
+const Footer = memo(() => {
+  return (
+    <footer>
+      <p>
+        memegle 2021. All rights reserved. Powered by <a href="https://giphy.com/">Giphy</a>.
+      </p>
+    </footer>
+  );
+});
 
 export default Footer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: "./src/index.js",
-  resolve: { extensions: [".js", ".jsx"] },
+  entry: './src/index.js',
+  resolve: { extensions: ['.js', '.jsx'] },
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/dist"),
-    clean: true
+    filename: 'bundle.[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    assetModuleFilename: 'static/[name][ext]',
+    path: path.join(__dirname, '/dist'),
+    clean: true,
   },
   devServer: {
     hot: true,
     open: true,
     historyApiFallback: true,
   },
-  devtool: "source-map",
+  devtool: isProduction ? false : 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./public", to: "./public" }]
+      patterns: [{ from: './public', to: './public' }],
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
   module: {
     rules: [
@@ -32,26 +36,27 @@ module.exports = {
         test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: "file-loader",
-        options: {
-          name: "static/[name].[ext]"
-        }
-      }
-    ]
+        test: /\.(jpg|webm)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
   optimization: {
-    minimize: false
-  }
+    splitChunks: {
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+        },
+      },
+    },
+  },
 };
