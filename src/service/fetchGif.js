3872a9568ccaf3ddf80600751e24d8a73e012b8a
Tanney102 <57767891+Tanney-102@users.noreply.github.com>
2021-09-02 10:15:46 +0900

    [성능 베이스캠프] 브콜(오태은) 미션 제출합니다. (#2)
    * chore: webpack bundle analyzer 추가
    
    * chore: terser minizer 적용
    
    * feat: 다이나믹 임포트 적용 및 청크 분리
    
    * chore: css minimizer 적용
    
    * chore: 번들 파일 압축(gzip)
    
    * feat: 이미지 사이즈 최적화
    
    - 이미지 사이즈, 용량, 메타데이터 최적화
    - srcset 적용
    
    * docs: 완료한 요구사항 반영
    
    * chore: 번들파일 해시 전략 수정
    
    * docs: 요구사항 반영
    
    * chore: 압축률을 높힌 이미지 적용
    
    * feat: fetch trending api 메모이제이션 적용
    
    * refactor: GifItem 메모이제이션 적용
    
    * refactor: 레이아웃 쉬프트가 없는 호버 애니메이션 적용
    
    * chore: eol 적용

--- src/service/fetchGif.js
+++ src/service/fetchGif.js
@@ -18,15 +18,24 @@ const formatResponse = (gifList) => {
     });
 }
 
-export const fetchTrendingGifs = () => {
-    return fetch(TRENDING_GIF_API)
-        .then(response => response.json())
-        .then(gifs => gifs.data)
-        .then(formatResponse)
-        .catch(e => {
-            return [];
-        });
-}
+export const fetchTrendingGifs = (() => {
+        const cache = { current: null };
+
+        return async () => {
+            try {
+                if (!cache.current) {
+                    const response = await fetch(TRENDING_GIF_API)
+                    const gifs = await response.json()
+
+                    cache.current = formatResponse(gifs?.data ?? []);
+                }
+
+                return cache.current ?? [];
+            } catch (error) {
+                return [];
+            }
+        }
+    })();
 
 export const fetchGifsByKeyword = (keyword, page = 0) => {
     const offset = page * DEFAULT_FETCH_COUNT;
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,20 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const  CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: process.env.NODE_ENV,
   resolve: { extensions: [".js", ".jsx"] },
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
+    chunkFilename: "[id].[contenthash].js",
     path: path.join(__dirname, "/dist"),
-    clean: true
+    clean: true,
   },
   devServer: {
     hot: true,
@@ -24,7 +30,15 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: "./public", to: "./public" }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      reportFilename: "docs/report.html",
+      openAnalyzer: false,
+    }),
+    new CompressionPlugin({
+      test: /\.(js|js\.map)?$/i
+    }),
   ],
   module: {
     rules: [
@@ -43,7 +57,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|jpeg|gif|webp)$/i,
         loader: "file-loader",
         options: {
           name: "static/[name].[ext]"
@@ -52,6 +66,39 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        defaultVendors: false,
+        react: {
+          chunks: "all",
+          name: "react",
+          test: /(?<!node_modules.*)[\/]node_modules[\/](react|react-dom|react-router-dom)[\/]/,
+          priority: 40,
+        },
+        asyncComponents: {
+          chunks: "async",
+          priority: 20,
+        },
+        duplicates: {
+          name: "duplicates",
+          minChunks: 2,
+          priority: 30,
+        },
+        commonVendors: {
+          chunks: "all",
+          name: 'commonVendors',
+          test: /[\/]node_modules[\/]/,
+          minChunks: 1,
+          priority: 10,
+        }
+      }
+    }
   }
 };
