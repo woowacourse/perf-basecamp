9f43480586116e511e5b96fbe0e5cd7993289f04
Chayan <hchayan196@gmail.com>
2021-09-02 11:13:54 +0900

    chore: output filename hash 정보 추가

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: './src/index.js',
   resolve: { extensions: ['.js', '.jsx'] },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[hash].bundle.js',
     path: path.join(__dirname, '/dist'),
     clean: true,
   },
