ffd6a14ef8c519c68e355643cf30f08153a191c5
Chayan <chayans999@gmail.com>
2021-09-02 15:34:37 +0900

    [성능 베이스캠프] 인치(한창희) 미션 제출합니다. (#11)
    * chore: webpack 분석을 위한 webpack-bundle-analyzer 모듈 설치
    
    * chore: uglifyJsPlugin 모듈 적용
    
    * chore: hero 이미지 리사이즈, 확장자변경, webpack 이미지 압축 구현
    
    * feat: loadable 모듈 설치 및 코드 스플리트 적용
    
    * chore: 중복 모듈사용 번들 분리 적용
    
    * refactor: 검색한요소 재랜더링 방지 React.memo 적용
    
    * chore: gif webpack 압축 옵션 추가
    
    * refactor: 제목요소 순차 개선, 이미지 속성 누락 추가, image 비율 자연스럽게 조절, meta 태그 추가
    
    * Create main.yml
    
    * chore: bundle-analyzer 웹페이지 실행 금지 설정
    
    * styles: heroImage 비율 유지위한 속성 추가
    
    * refactor: asset gif 용량 압축
    
    * refactor: trending.gif resize
    
    * refactor: 불러오는 gif alt 속성 추가
    
    * chore: robots.txt 구현
    
    * chore: github action GIPHY env file 생성
    
    * refactor: trendingGif 요청 12시간마다 수행하게끔 localstorage 적용
    
    * refactor: css hover시 변화 layout shift 문제해결
    
    * chore: CI/CD 구문에서 불필요한 touch 명령어 제거
    
    * refactor: 기존 제목 태그로 원상 복귀
    
    * chore: output filename hash 정보 추가
    
    * chore: terser default 동작을 위한 uglifyJS 플러그인 제거
    
    * chore: webpack devtool 명시 제거

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  resolve: { extensions: [".js", ".jsx"] },
+  entry: './src/index.js',
+  resolve: { extensions: ['.js', '.jsx'] },
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/dist"),
-    clean: true
+    filename: '[name].[hash].bundle.js',
+    path: path.join(__dirname, '/dist'),
+    clean: true,
   },
   devServer: {
     hot: true,
     open: true,
     historyApiFallback: true,
   },
-  devtool: "source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./public", to: "./public" }]
+      patterns: [{ from: './public', to: './public' }],
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin({ analyzerMode: 'disabled' }),
+    new ImageMinimizerPlugin({
+      exclude: /node_modules/,
+      minimizerOptions: {
+        plugins: [
+          ['gifsicle', { interlaced: true, optimizationLevel: 3 }],
+          ['mozjpeg', { quality: 70 }],
+          ['pngquant', { optimizationLevel: 7 }],
+          ['svgo', { plugins: [{ removeViewBox: false }] }],
+          ['imagemin-webp'],
+        ],
+      },
+    }),
   ],
   module: {
     rules: [
@@ -32,26 +48,30 @@ module.exports = {
         test: /\.(js|jsx)$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: "file-loader",
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
+        loader: 'file-loader',
         options: {
-          name: "static/[name].[ext]"
-        }
-      }
-    ]
+          name: 'static/[name].[ext]',
+        },
+      },
+    ],
   },
   optimization: {
-    minimize: false
-  }
+    splitChunks: {
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
