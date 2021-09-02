0553a5d85723cddd76a6bf633236ac48ece38b04
sunhpark42 <67677561+sunhpark42@users.noreply.github.com>
2021-09-02 19:10:45 +0900

    Revert "[성능 베이스캠프] 미키(권세진) 미션 제출합니다." (#27)
    * Revert "[성능 베이스캠프] 미키(권세진) 미션 제출합니다. (#14)"
    
    This reverts commit 91a018a176aeb040c6efcc1a045803431923e430.
    
    * refactor: navBar에 React.memo 적용
    
    Co-authored-by: Kwon Se-jin <48755175+0307kwon@users.noreply.github.com>

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 import styles from "./NavBar.module.css";
 
-const NavBar = () => {
-    return (
-        <header className={styles.header}>
-            <nav className={styles.nav}>
-                <Link to="/">
-                    <span className={styles.logo}>memegle</span>
-                </Link>
-                <Link to="/search">
-                    <button className={styles.searchPageButton}>start search</button>
-                </Link>
-            </nav>
-        </header>
-    );
-}
+const NavBar = React.memo(function NavBar() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link to="/">
+          <span className={styles.logo}>memegle</span>
+        </Link>
+        <Link to="/search">
+          <button className={styles.searchPageButton}>start search</button>
+        </Link>
+      </nav>
+    </header>
+  );
+});
 
 export default NavBar;
