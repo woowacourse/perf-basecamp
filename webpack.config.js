e40f2f46f52860e764cd7bea5a7dd5352a8ae1ef
Min Kyung Kang <mnk918p@gmail.com>
2021-09-02 13:50:37 +0900

    chore: deprecated된 plugin 변경
    - dev, production 모드인지 확인해 css loader를 다르게 줌
    - build file name 변경

--- webpack.config.js
+++ webpack.config.js
@@ -2,93 +2,103 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  resolve: { extensions: ['.js', '.jsx'] },
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/dist'),
-    clean: true,
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      minify: {
-        collapseWhiteSpace: true,
-        removeComments: true,
-      },
-      hash: true,
-      template: './index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './public', to: './public' }],
-    }),
-    new MiniCssExtractPlugin({
-      filename: `[name].css`,
-    }),
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /\.(js|css)$/,
-    }),
-    new ImageminWebpWebpackPlugin(),
-    new Dotenv(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = () => {
+  const mode = process.env.NODE_ENV || 'development';
+
+  return {
+    entry: './src/index.js',
+    resolve: { extensions: ['.js', '.jsx'] },
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[id].[chunkhash].js',
+      path: path.join(__dirname, '/dist'),
+      clean: true,
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+    },
+    devtool: 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        minify: {
+          collapseWhiteSpace: true,
+          removeComments: true,
         },
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]',
-        },
-      },
-      {
-        test: /\.mp4$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'video',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|webp|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'static/[name].[ext]',
-            },
-          },
-        ],
-      },
+        hash: true,
+        template: './index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './public', to: './public' }],
+      }),
+      new MiniCssExtractPlugin({
+        filename: `[name].css`,
+      }),
+      new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.(js|css)$/,
+      }),
+      new webpack.DefinePlugin({}),
+      new Dotenv(),
     ],
-  },
-  optimization: {
-    minimizer: [new OptimizeCSSAssetsPlugin(), '...'],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'static/[name].[ext]',
+          },
+        },
+        {
+          test: /\.mp4$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'video',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|webp|svg|webp)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'static/[name].[ext]',
+                quality: 25,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: { chunks: 'all' },
+      minimizer: [new CssMinimizerPlugin(), '...'],
+    },
+  };
 };
