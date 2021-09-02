b68183c4c8552f2767ae697afb227a704a2bcf55
jum0 <wnsah052@naver.com>
2021-09-02 17:30:59 +0900

    refactor: viewport에 따라 다른 이미지를 불러오도록 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,9 @@ import NavBar from '../../components/NavBar/NavBar';
 import Footer from '../../components/Footer/Footer';
 import FeatureItem from '../../components/FeatureItem/FeatureItem';
 
-import heroImage from '../../assets/images/hero.webp';
+import heroImage768 from '../../assets/images/hero-768.webp';
+import heroImage1024 from '../../assets/images/hero-1024.webp';
+import heroImage1600 from '../../assets/images/hero-1600.webp';
 import trendingMp4 from '../../assets/images/trending.mp4';
 import findMp4 from '../../assets/images/find.mp4';
 
@@ -16,7 +18,12 @@ const Home = () => {
     <>
       <NavBar />
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero" />
+        <img
+          className={styles.heroImage}
+          srcset={`${heroImage768} 768w, ${heroImage1024} 1024w, ${heroImage1600} 1600w`}
+          sizes="(max-width: 768px) 768px, (max-width: 1024px) 1024px, 100vw"
+          alt="hero"
+        />
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
