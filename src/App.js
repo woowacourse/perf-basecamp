abe14e7ac4025dab107d01c94239ec5626cda832
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-09-02 23:20:47 +0900

    refactor: import 구문 정리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import "./App.css";
+
 const HomePage = lazy(() => import("./pages/Home/Home"));
 const SearchPage = lazy(() => import("./pages/Search/Search"));
-
-import "./App.css";
 
 const App = () => {
   return (
