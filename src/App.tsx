daf25a7e18ae373d58c6ca89ecf97cfd2c2653d0
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-31 11:08:51 +0900

    refactor : 검색 페이지 code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+const Search = React.lazy(() => import('./pages/Search/Search'));
+
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<div>로딩 중</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
