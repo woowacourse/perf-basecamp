02618936e443d19d96d150e17e2a79e1895a1aeb
Hwang Jung Min <hwangstar156@gmail.com>
2022-09-02 00:03:55 +0900

    feat: code spliting을 위한 react.lazt 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import React from 'react';
+import Home from './pages/Home/Home';
+
+const Search = React.lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <React.Suspense fallback={<div>로딩중..</div>}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </React.Suspense>
   );
 };
 
