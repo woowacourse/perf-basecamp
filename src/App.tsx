925ee846571a3457a28776785e2c0730e422c3b1
moonheekim0118 <hellomooneekim@gmail.com>
2022-09-01 18:00:31 +0900

    refactor: 요청 크기를 줄인다. - 코드 스플릿팅을 적용하여, Home Page 에서 불러오는 스크립트 리소스에 gif 검색을 위한 giphy 모듈이 포함되어 있지 않도록 구현 - Suspense 를 사용하여 해당 페이지 레이지로딩 하는 동안 로딩중이라는 안내메시지 보여주도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+
+const Search = React.lazy(() => import('./pages/Search/Search'));
 
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Suspense fallback={<h1>로딩 중 입니다.</h1>}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
