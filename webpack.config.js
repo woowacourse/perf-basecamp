88546931d15d994b5d6cf82c54639b28a29b4db6
airman5573 <rpf5573@kakao.com>
2022-09-02 15:14:54 +0900

    aggresive webpack plugin 적용

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const AggresiveWebpackPlugin = require('webpack/lib/optimize/AggressiveSplittingPlugin');
 
 module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
     path: path.join(__dirname, '/dist'),
+    filename: '[chunkhash].js',
+    chunkFilename: '[chunkhash].js',
     clean: true
   },
   devServer: {
@@ -16,7 +18,6 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
@@ -24,7 +25,14 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new AggresiveWebpackPlugin({
+      // https://webpack-v3.jsx.app/plugins/aggressive-splitting-plugin/
+      minSize: 30000, // Byte, split point. Default: 30720
+      maxSize: 50000, // Byte, maxsize of per file. Default: 51200
+      chunkOverhead: 0, // Default: 0
+      entryChunkMultiplicator: 1 // Default: 1
+    })
   ],
   module: {
     rules: [
