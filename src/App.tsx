174b0c85aefea9dc7aba2681c1b564ca5c256116
Yo Wook Kim <uk960214@gmail.com>
2022-09-02 17:48:48 +0900

    refactor: footer 레이아웃 시프트 방지 조치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
-
 const Home = lazy(() => import(/* webpackChunkName: "Home" */ './pages/Home/Home'));
 const Search = lazy(() => import(/* webpackChunkName: "Search" */ './pages/Search/Search'));
+
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 import './App.css';
 
@@ -13,12 +13,14 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </Suspense>
+      <div style={{ minHeight: '100vh' }}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </Suspense>
+      </div>
       <Footer />
     </Router>
   );
