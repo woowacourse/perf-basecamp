557a0d3341cddaab160f83fc72cf570640568595
junzzi <jjh01027717502@gmail.com>
2022-09-03 01:12:15 +0900

    feat: 소스코드, 이미지 크기 줄이기

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -2,13 +2,17 @@ import styles from './FeatureItem.module.css';
 
 type FeatureItemProps = {
   title: string;
-  imageSrc: string;
+  videoWebmSrc: string;
+  videoMp4Src: string;
 };
 
-const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
+const FeatureItem = ({ title, videoWebmSrc, videoMp4Src }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      <video className={styles.featureImage} autoPlay loop muted playsInline>
+        <source src={videoWebmSrc} type="video/webm" />
+        <source src={videoMp4Src} type="video/webm" />
+      </video>
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
