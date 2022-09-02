a585e443680d2c613e669bca5559382fab835fed
wonsss <bigdatajang@gmail.com>
2022-09-03 00:11:32 +0900

    feat: api Get 요청(trending, search)에 캐싱 유틸 적용
    선 캐시, 후 네트워크

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -1,5 +1,6 @@
 import { GifsResult, GiphyFetch, SearchOptions, TrendingOptions } from '@giphy/js-fetch-api';
 import { IGif } from '@giphy/js-types';
+import { cache } from '../cache';
 
 import { GifImageModel } from '../models/image/gifImage';
 
@@ -33,7 +34,8 @@ export const gifAPIService = {
     };
 
     try {
-      const gifs: GifsResult = await gf.trending(trendingOptions);
+      const gifs: GifsResult = await cache('trending', () => gf.trending(trendingOptions));
+
       return convertResponseToModel(gifs.data);
     } catch (e) {
       return [];
@@ -49,12 +51,14 @@ export const gifAPIService = {
   searchByKeyword: async function (keyword: string, page: number): Promise<GifImageModel[]> {
     const searchOptions: SearchOptions = {
       limit: DEFAULT_FETCH_COUNT,
-      lang: 'ko',
+      lang: 'En',
       offset: page * DEFAULT_FETCH_COUNT
     };
 
     try {
-      const gifs: GifsResult = await gf.search(keyword, searchOptions);
+      const gifs: GifsResult = await cache(`search-${keyword}`, () =>
+        gf.search(keyword, searchOptions)
+      );
       return convertResponseToModel(gifs.data);
     } catch (e) {
       return [];
--- webpack.config.js
+++ webpack.config.js
@@ -55,15 +55,6 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true
-          }
-        }
-      }),
-      new OptimizeCSSAssetsPlugin()
-    ]
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()]
   }
 };
