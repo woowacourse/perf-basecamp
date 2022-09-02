dd3dabc671c748607d72286f0ff5e35620d49553
junzzi <jjh01027717502@gmail.com>
2022-09-03 01:13:08 +0900

    feat: 필요한 것만 불러오기, react-icons 트리쉐이킹, 레이지 로딩 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+
+const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<div>Loading</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
