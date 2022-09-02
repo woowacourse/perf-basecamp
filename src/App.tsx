b2880887e41e4af6034f21848ce52040ec123523
airman5573 <rpf5573@kakao.com>
2022-09-02 15:14:38 +0900

    검색 페이지 lazy loading 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import React from 'react';
+
+const Search = React.lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
@@ -14,7 +16,14 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search"
+          element={
+            <React.Suspense fallback={<>loading...</>}>
+              <Search />
+            </React.Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
