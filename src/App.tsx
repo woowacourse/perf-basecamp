bf822e215ecf69384f3947c96a49f360fe7c9b04
kwannee <yleh1126@naver.com>
2022-09-02 16:48:32 +0900

    refactor: Search만 split

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from './pages/Home/Home';
+
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
-const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
