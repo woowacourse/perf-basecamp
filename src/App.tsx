7e570e39c0d2d0853598ba59cb74d2e779bf06ea
JUDONGHYEOK <judonghyeok@gmail.com>
2022-09-03 11:29:02 +0900

    chore: home page 및 search 페이지 레이지 로딩

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { Suspense } from 'react';
 
-// Old approach for getting URL
-// import webp from './assets/images/hero.jpg?as=webp';
-
-// Assets modules
-// console.log(new URL('./assets/images/hero.png?as=webp'));
-
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
+const Home = React.lazy(() => import('./pages/Home/Home'));
+const Search = React.lazy(() => import('./pages/Search/Search'));
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
@@ -18,10 +13,12 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<div></div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -28,7 +32,11 @@ module.exports = {
       patterns: [{ from: './public', to: './public' }]
     }),
     new Dotenv(),
-    new ImageminWebpWebpackPlugin()
+    new MiniCssExtractPlugin(),
+    new ImageminWebpWebpackPlugin(),
+    new CompressionPlugin({ algorithm: 'gzip' }),
+    new BundleAnalyzerPlugin(),
+    new CleanWebpackPlugin()
   ],
 
   module: {
@@ -44,44 +52,44 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: ImageMinimizerPlugin.loader,
-        enforce: 'pre',
-        options: {
-          generator: [
-            {
-              preset: 'webp',
-              implementation: ImageMinimizerPlugin.squooshGenerate,
-              options: {
-                plugins: ['imagemin-webp'],
-                encodeOptions: {
-                  // Please specify only one codec here, multiple codecs will not work
-                  resize: {
-                    enabled: true,
-                    width: 100,
-                    height: 50
-                  },
-                  encodeOptions: {
-                    webp: {
-                      quality: 40
-                    }
-                  }
-                }
-              }
-            }
-            // {
-            //   preset: 'png',
-            //   implementation: ImageMinimizerPlugin.imageminGenerate,
-            //   options: {
-            //     plugins: ['imagemin-pngquant']
-            //   }
-            // }
-          ]
-        }
-      },
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   loader: ImageMinimizerPlugin.loader,
+      //   enforce: 'pre',
+      //   options: {
+      //     generator: [
+      //       {
+      //         preset: 'webp',
+      //         implementation: ImageMinimizerPlugin.squooshGenerate,
+      //         options: {
+      //           plugins: ['imagemin-webp'],
+      //           encodeOptions: {
+      //             // Please specify only one codec here, multiple codecs will not work
+      //             resize: {
+      //               enabled: true,
+      //               width: 100,
+      //               height: 50
+      //             },
+      //             encodeOptions: {
+      //               webp: {
+      //                 quality: 40
+      //               }
+      //             }
+      //           }
+      //         }
+      //       }
+      //       // {
+      //       //   preset: 'png',
+      //       //   implementation: ImageMinimizerPlugin.imageminGenerate,
+      //       //   options: {
+      //       //     plugins: ['imagemin-pngquant']
+      //       //   }
+      //       // }
+      //     ]
+      //   }
+      // },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
         type: 'asset'
@@ -90,77 +98,33 @@ module.exports = {
   },
   optimization: {
     minimize: true,
+
     minimizer: [
       '...',
+      new ImageminWebpWebpackPlugin({
+        generator: [
+          {
+            preset: 'webp',
+            implementation: ImageminWebpWebpackPlugin.imageminGenerate,
+            options: {
+              plugins: [['webp', { quality: 30 }]]
+            }
+          }
+        ],
+        minimizer: {
+          implementation: ImageminWebpWebpackPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['pngquant', { optimizationLevel: 20 }] // lossless PNG optimization
+            ]
+          }
+        }
+      }),
+      new TerserPlugin({}),
       new ESBuildMinifyPlugin({
         target: 'es2015',
         css: true // Apply minification to CSS assets
       })
-      // new ImageMinimizerPlugin({
-      //   minimizer: {
-      //     implementation: ImageMinimizerPlugin.imageminMinify,
-      //     options: {
-      //       plugins: [
-      //         // ['gifsicle', { interlaced: true, optimizationLevel: 3, colors: 64 }],
-      //         ['pngquant', { speed: 3, strip: true, quality: [0.1, 0.3], dithering: 0.1 }],
-      //         ['webp', { quality: 50, resize: { width: 1280, height: 0 } }]
-      //       ]
-      //     }
-      //   }
-      // })
-      // new ImageMinimizerPlugin({
-      //   // generator: [
-      //   //   {
-      //   //     type: 'asset',
-      //   //     implementation: ImageMinimizerPlugin.imageminGenerate,
-      //   //     options: {
-      //   //       plugins: ['imagemin-pngquant', 'imagemin-webp']
-      //   //     }
-      //   //   }
-      //   // ],
-      //   // generator: [
-      //   //   {
-      //   //     // You can apply generator using `?as=avif`, you can use any name and provide more options
-      //   //     preset: 'avif',
-      //   //     implementation: ImageMinimizerPlugin.squooshGenerate,
-      //   //     options: {
-      //   //       encodeOptions: {
-      //   //         avif: {
-      //   //           cqLevel: 33
-      //   //         }
-      //   //       }
-      //   //     }
-      //   //   },
-      //   //   {
-      //   //     // You can apply generator using `?as=webp`, you can use any name and provide more options
-      //   //     preset: 'webp',
-      //   //     implementation: ImageMinimizerPlugin.squooshGenerate,
-      //   //     options: {
-      //   //       encodeOptions: {
-      //   //         // Please specify only one codec here, multiple codecs will not work
-      //   //         webp: {
-      //   //           quality: 100
-      //   //         },
-      //   //         pngquant: {
-      //   //           quality: 10
-      //   //         }
-      //   //       }
-      //   //     }
-      //   //   }
-      //   // ],
-      // new ImageMinimizerPlugin({
-      //   minimizer: {
-      //     implementation: ImageMinimizerPlugin.squooshMinify,
-      //     options: {
-      //       plugins: [
-      //         ['gifsicle', { interlaced: true }],
-      //         ['optipng', { optimizationLevel: 10, quality: 10 }],
-      //         ['webp', { quality: 50, resize: { width: 1280, height: 0 } }]
-      //       ]
-      //     }
-      //   }
-      // })
-      // })
     ]
   }
 };
