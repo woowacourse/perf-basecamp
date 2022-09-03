11cb90bd690e2833fd8a4550e7640ffba38a0f35
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-09-03 19:31:49 +0900

    chore: lazy import 부분 변경
    - 필요없는 구문 삭제
    - suspense를 lazy import되는 컴포넌트를 감싸게 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from './pages/Home/Home';
+
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 import { lazy, Suspense } from 'react';
 
-const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
     <Router>
-      <Suspense fallback={<div>loading...</div>}>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-        <Footer />
-      </Suspense>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/search"
+          element={
+            <Suspense fallback={<div>loading...</div>}>
+              <Search />
+            </Suspense>
+          }
+        />
+      </Routes>
+      <Footer />
     </Router>
   );
 };
