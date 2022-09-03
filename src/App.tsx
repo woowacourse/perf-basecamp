c9387e50268b16a9d3a9b3365b9cff41fdfaf04f
airman5573 <rpf5573@kakao.com>
2022-09-04 03:37:16 +0900

    Home도 code spliting 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 import React from 'react';
 
+const Home = React.lazy(() => import('./pages/Home/Home'));
 const Search = React.lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
@@ -15,7 +14,14 @@ const App = () => {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <React.Suspense fallback={<>loading...</>}>
+              <Home />
+            </React.Suspense>
+          }
+        />
         <Route
           path="/search"
           element={
