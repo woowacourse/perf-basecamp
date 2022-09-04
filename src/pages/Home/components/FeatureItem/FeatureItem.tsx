22f67cb6d93d05a49d182ced1dd55f2e93278845
kkojae <jaejeung3210@gmail.com>
2022-09-04 19:21:54 +0900

    refactor: FeatureItem Component Props 수정 및 d.ts mp4 추가

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -2,14 +2,16 @@ import styles from './FeatureItem.module.css';
 
 type FeatureItemProps = {
   title: string;
-  imageSrc: string;
+  defaultVideoSrc: string;
+  subVideoSrc: string;
 };
 
-const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
+const FeatureItem = ({ title, defaultVideoSrc, subVideoSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
       <video autoPlay loop muted playsInline>
-        <source src={imageSrc} type="video/webm" />
+        <source src={defaultVideoSrc} type="video/webm" />
+        <source src={subVideoSrc} type="video/mp4" />
       </video>
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -4,3 +4,4 @@ declare module '*.gif';
 declare module '*.svg';
 declare module '*.webp';
 declare module '*.webm';
+declare module '*.mp4';
