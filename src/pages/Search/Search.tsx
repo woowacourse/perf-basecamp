dd7749680f68735df83da3c316ec13770c96a6ee
kimsanglok <evertree6031@gmail.com>
2022-09-05 04:24:32 +0900

    feat: HelpPanel - 대표 이미지 preloading 적용

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -19,6 +19,15 @@ const Search = () => {
     searchByKeyword();
   };
 
+  const handleMouseOverHelpPanelButton = () => {
+    const MainImage = new Image();
+    const BannerImage = new Image();
+
+    MainImage.src =
+      'https://media0.giphy.com/media/3oKIPdiPGxPI7Dze7u/giphy.gif?cid=ecf05e475f5bct6ci09g3pgn43nf6bausx33fj7f96f6ig92&rid=giphy.gif&ct=g';
+    BannerImage.src = 'https://giphy.com/static/img/artistdirectory_1040.gif';
+  };
+
   return (
     <div className={styles.searchContainer}>
       <SearchBar
@@ -28,7 +37,7 @@ const Search = () => {
         onSearch={searchByKeyword}
       />
       <SearchResult status={status} gifList={gifList} loadMore={loadMore} />
-      <HelpPanel />
+      <HelpPanel handleMouseOver={handleMouseOverHelpPanelButton} />
     </div>
   );
 };
