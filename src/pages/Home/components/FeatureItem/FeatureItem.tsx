b13b1fd10a27438541d0c2652ae77705442d902f
(yunjin) <sion11290805@gmail.com>
2022-09-04 14:45:42 +0900

    feat: img 태그 video 태그로 변경

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -8,7 +8,9 @@ type FeatureItemProps = {
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      <video autoPlay loop muted className={styles.featureImage}>
+        <source src={imageSrc} type="video/webm" />
+      </video>
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
