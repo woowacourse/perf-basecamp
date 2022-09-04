cfc79eebdd39c737eeaee5d03185efec0af7181c
gytks4 <gytks4@naver.com>
2022-09-04 16:10:27 +0900

    chore: 요구사항 변경에 따른 API 통신 코드 수정

--- src/pages/Search/hooks/useGifSearch.tsx
+++ src/pages/Search/hooks/useGifSearch.tsx
@@ -14,10 +14,6 @@ export const SEARCH_STATUS = {
 
 export type SearchStatus = typeof SEARCH_STATUS[keyof typeof SEARCH_STATUS];
 
-const refetchDuration = 1000 * 60 * 10;
-let statusRef: null | 'LOADING' = null;
-let timerRef: NodeJS.Timeout | null = null;
-
 const useGifSearch = () => {
   const [status, setStatus] = useState<SearchStatus>(SEARCH_STATUS.BEFORE_SEARCH);
   const [currentPageIndex, setCurrentPageIndex] = useState(DEFAULT_PAGE_INDEX);
@@ -60,24 +56,13 @@ const useGifSearch = () => {
 
   useEffect(() => {
     const fetch = async () => {
-      if (statusRef !== 'LOADING') {
+      if (status === SEARCH_STATUS.BEFORE_SEARCH) {
         const gifs: GifImageModel[] = await gifAPIService.getTrending();
 
         setGifList(gifs);
       }
     };
     fetch();
-
-    return () => {
-      statusRef = SEARCH_STATUS.LOADING;
-
-      if (timerRef === null) {
-        timerRef = setTimeout(() => {
-          statusRef = null;
-          timerRef = null;
-        }, refetchDuration);
-      }
-    };
   }, []);
 
   return {
