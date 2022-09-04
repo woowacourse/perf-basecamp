841ed5a4f37fad1d0326968da925639924b69e4d
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-04 22:14:57 +0900

    refactor: code-spliiting을 통해, Home 페이지에 불러오는 리소스 줄이기

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import { Suspense, lazy } from 'react';
 
 import './App.css';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<div>loading</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
