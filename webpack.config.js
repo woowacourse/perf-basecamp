ea098aee2c314210b454bb00fe8370c13e9b3fce
prefer2 <sunho6200@gmail.com>
2022-09-04 22:23:38 +0900

    refactor: 불필요한 plugin 제거

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -62,14 +61,6 @@ module.exports = {
     minimizer: [
       '...',
       new CssMinimizerPlugin(),
-      new TerserPlugin({
-        parallel: true, // 병렬 처리를 통해 속도 개선
-        terserOptions: {
-          compress: {
-            drop_console: true // 콘솔 로그를 지우는 옵션
-          }
-        }
-      }),
       new ImageMinimizerPlugin({
         minimizer: {
           implementation: ImageMinimizerPlugin.imageminGenerate,
