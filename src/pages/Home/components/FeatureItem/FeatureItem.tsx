7261f8066f55e783f47ff3d22f28758d23b55949
wonsss <bigdatajang@gmail.com>
2022-09-06 00:53:11 +0900

    refactor: imageSrc 네이밍을 videoSrc로 변경

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -2,14 +2,14 @@ import styles from './FeatureItem.module.css';
 
 type FeatureItemProps = {
   title: string;
-  imageSrc: string;
+  videoSrc: string;
 };
 
-const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
+const FeatureItem = ({ title, videoSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
       <video className={styles.featureImage} autoPlay loop muted playsInline>
-        <source src={imageSrc} type="video/mp4" />
+        <source src={videoSrc} type="video/mp4" />
       </video>
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
