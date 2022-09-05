0ee3eb1de6b4f0675531fbd26af8a94da0b48d15
Yo Wook Kim <uk960214@gmail.com>
2022-09-05 17:26:41 +0900

    refactor: custom cursor의 애니메이션을 rAF를 활용해 구현

--- src/pages/Home/components/CustomCursor/CustomCursor.tsx
+++ src/pages/Home/components/CustomCursor/CustomCursor.tsx
@@ -11,12 +11,23 @@ const CustomCursor = ({ text = '' }: CustomCursorProps) => {
   const [...cursorTextChars] = text;
   const mousePosition = useMousePosition();
   const cursorRef = useRef<HTMLDivElement>(null);
+  const rAF = useRef<number | null>(null);
 
   useEffect(() => {
-    if (cursorRef.current) {
-      cursorRef.current.style.transform = `translate(${mousePosition.pageX}px, ${mousePosition.pageY}px)`;
-    }
-  }, [mousePosition]);
+    const cursorAnimationFrame = () => {
+      if (cursorRef.current) {
+        cursorRef.current.style.transform = `translate(${mousePosition.pageX}px, ${mousePosition.pageY}px)`;
+      }
+      requestAnimationFrame(cursorAnimationFrame);
+    };
+
+    rAF.current = requestAnimationFrame(cursorAnimationFrame);
+
+    return () => {
+      if (!rAF.current) return;
+      cancelAnimationFrame(rAF.current);
+    };
+  }, [mousePosition, cursorRef.current]);
 
   return (
     <div ref={cursorRef} className={styles.cursor}>
