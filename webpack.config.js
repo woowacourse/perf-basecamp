27eac16a1d2022fbf9faf7c45d62d1018995cdaa
sanaandmomo <qajjang46@naver.com>
2022-09-05 17:30:57 +0900

    build: production 환경에서는 소스맵 파일 미생성

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
+  devtool: false,
   plugins: [
     new BundleAnlyzerPlugin(),
     new HtmlWebpackPlugin({
