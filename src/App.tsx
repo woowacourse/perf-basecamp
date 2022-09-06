95be5eb7880a5863566125ea764495e4f6959207
Hwang Jung Min <hwangstar156@gmail.com>
2022-09-06 10:50:14 +0900

    feat: 코드스플리팅 부분개선

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
-import Home from './pages/Home/Home';
 
+const Home = React.lazy(() => import('./pages/Home/Home'));
 const Search = React.lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
-    <React.Suspense fallback={<div>로딩중..</div>}>
-      <Router>
-        <NavBar />
+    <Router>
+      <NavBar />
+      <React.Suspense fallback={<div>로딩중..</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
         </Routes>
-        <Footer />
-      </Router>
-    </React.Suspense>
+      </React.Suspense>
+      <Footer />
+    </Router>
   );
 };
 
