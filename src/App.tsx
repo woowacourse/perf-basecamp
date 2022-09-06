e1a28985fa82ebbd89c2686073755818dd2000e5
Ahn <tn3748@naver.com>
2022-09-07 03:16:12 +0900

    refactor: home lazu 추가 및 suspense 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
+const Home = React.lazy(() => import('./pages/Home/Home'));
 const Search = React.lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Router>
-        <NavBar />
+    <Router>
+      <NavBar />
+      <Suspense fallback={<div>loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
         </Routes>
-        <Footer />
-      </Router>
-    </Suspense>
+      </Suspense>
+      <Footer />
+    </Router>
   );
 };
 
