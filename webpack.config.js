a3eeaa036b203cb47b958963d6d6e26014dc6306
TaeYoon <uni613@naver.com>
2022-09-07 15:40:44 +0900

    [성능 베이스캠프 미션] 태태(김태윤) 미션 제출합니다. (#47)
    * chore: 모듈 설치
    
    * perf: 웹팩 minimize 옵션 활성화
    
    minify + uglify
    번들 파일의 사이즈를 줄여준다.
    
    * perf: 압축한 이미지로 교체
    
    * perf: hero.png -> hero.webp
    
    webp 파일로 바꾼 후 이미지 리사이징(1440*961)
    
    * perf: react-icons에서 필요한 아이콘만 불러오기
    
    * fix: gifAPIService코드 수정
    
    * perf: css minify
    
    기존의 style-loader는 css 모듈을 style 태그로 html에 주입하는 방식
    css minify를 위해 css-minimizer 플러그인을 설치하고 mini-css-extract 플러그인으로 style-loader를 대체하여 css 파일을 따로 번들링하도록 설정
    
    * perf: Search 페이지 파일 로딩 지연 적용
    
    Search 페이지 컴포넌트에 dynamic-import를 적용하여 웹팩 번들링 때 코드 스플리팅이 발생하도록 함
    
    * perf: GifItem 리렌더링 방지
    
    * feat: gifAPIService-getTrending 메서드에 캐싱 적용
    
    * perf: layout shift를 발생시키는 스타일 요소 수정
    
    - CustomCursor
    - GiftItem: hover
    - HelpPanel open&close
    
    * chore: 웹팩 콘피그 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -16,7 +18,6 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
@@ -24,7 +25,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
@@ -37,10 +39,10 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
@@ -49,6 +51,6 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimizer: [`...`, new CssMinimizerPlugin()]
   }
 };
