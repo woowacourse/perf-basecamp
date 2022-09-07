4170390c5b0d12788305513a4414a8e228fd2385
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-09-07 17:56:06 +0900

    [성능 베이스캠프 미션] 해리(엄지혁) 미션 제출합니다. (#54)
    * docs: 요구사항 작성
    
    * chore: webpack 설정 및 플러그인 install
    
    * feat: png, gif 파일 크기 줄이기
    
    * feat: png, gif 파일 크기 줄이기
    
    * feat: 소스코드 크기 줄이기
    
    * fix: 미션 문제 코드  수정
    
    * feat: 번들 파일 분리하기
    
    * feat: tree shaking 적용
    
    * feat: trending API를 페이지로 들어올 때마다 새로 요청하지 않도록 구현
    
    * fix: cache 저장 전에 gifs가 불러와지지않던 버그 수정
    
    * refactor: hero 이미지 120kb 아래로 빌드 되도록 수정
    
    * feat: CDN을 적용, 한 번 요청한 리소스는 CDN 캐시에서 불러와야 한다
    
    * feat: 검색 결과 > 추가 로드시 추가된 목록만 새로 렌더되어야 한다.
    
    * feat: mouse 애니메이션 Layout Shift 방지
    
    * docs: 미션 요구사항 완료 (캐시 정책 외)
    
    * feat: 놓친 애니메이션 최적화
    
    * chore: 번들파일에 contenthash 추가
    
    * chore: console.log 제거

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -1,4 +1,4 @@
-import { GifsResult, GiphyFetch, SearchOptions, TrendingOptions } from '@giphy/js-fetch-api';
+import { GifsResult, GiphyFetch, SearchOptions } from '@giphy/js-fetch-api';
 import { IGif } from '@giphy/js-types';
 
 import { GifImageModel } from '../models/image/gifImage';
@@ -7,6 +7,7 @@ const apiKey = process.env.GIPHY_API_KEY || '';
 const gf = new GiphyFetch(apiKey);
 
 const DEFAULT_FETCH_COUNT = 16;
+const TRENDING_GIF_API = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=${DEFAULT_FETCH_COUNT}&rating=g`;
 
 function convertResponseToModel(gifList: IGif[]): GifImageModel[] {
   return gifList.map((gif) => {
@@ -26,14 +27,19 @@ export const gifAPIService = {
    * @returns {Promise<GifImageModel[]>}
    * @ref https://developers.giphy.com/docs/api/endpoint#!/gifs/trending
    */
-  getTrending: async function (): Promise<GifImageModel[]> {
-    const trendingOptions: TrendingOptions = {
-      limit: DEFAULT_FETCH_COUNT,
-      rating: 'g'
-    };
 
+  getTrending: async function (): Promise<GifImageModel[]> {
     try {
-      const gifs: GifsResult = await gf.trending(trendingOptions);
+      const cacheStorage = await caches.open('trending');
+      const cacheGifs = await cacheStorage
+        .match(TRENDING_GIF_API)
+        .then((res) => (res ? res.json() : undefined));
+
+      if (cacheGifs) return convertResponseToModel(cacheGifs.data);
+
+      const gifs: GifsResult = await fetch(TRENDING_GIF_API).then((res) => res.json());
+      await cacheStorage.add(TRENDING_GIF_API);
+
       return convertResponseToModel(gifs.data);
     } catch (e) {
       return [];
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].bundle.js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
   devServer: {
     hot: true,
     open: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    port: 9000
   },
   devtool: 'source-map',
   plugins: [
@@ -24,6 +27,11 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      exclude: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|txt|map|ico)$/i
+    }),
+    //new BundleAnalyzerPlugin(),
     new Dotenv()
   ],
   module: {
@@ -40,15 +48,26 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
         }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|webp)$/i,
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            quality: 40
+          },
+          gifsicle: {
+            interlaced: true,
+            optimizationLevel: 3,
+            colors: 32
+          }
+        }
       }
     ]
-  },
-  optimization: {
-    minimize: false
   }
 };
