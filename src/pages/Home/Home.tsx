bb9f2b0f30212d64ce47afdc9c5e7816cca35edd
Ashley Heo <ashleysyheo@gmail.com>
2023-08-31 13:42:34 +0900

    refactor: 이미지 최적화된 파일로 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,8 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
+import heroImage_png from '../../assets/images/hero.png';
+import heroImage_webp from '../../assets/images/hero.webp';
 import trendingGif from '../../assets/images/trending.gif';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
@@ -19,7 +20,10 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source srcSet={heroImage_webp} type="image/webp" media="all" />
+          <img className={styles.heroImage} src={heroImage_png} alt="hero" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
