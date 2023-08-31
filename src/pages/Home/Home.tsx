77639ac9ca942b2dc51fb46623dbec5c2a18d73f
요술토끼 <curious.wzrabbit@gmail.com>
2023-09-01 02:53:56 +0900

    ♻️ refactor: 메인 페이지의 이미지, 영상에 대한 성능 개선
    - 항상 적합한 크기의 이미지를 보여주도록 개선
    - 용량이 큰 gif 이미지 대신 mp4 동영상을 보여주도록 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroSmallImage from '../../assets/images/hero_small.webp';
+import heroMediumImage from '../../assets/images/hero_medium.webp';
+import heroLargeImage from '../../assets/images/hero_large.webp';
+import heroExtraLargeImage from '../../assets/images/hero_extra_large.webp';
+import trendingVideo from '../../assets/images/trending.mp4';
+import findVideo from '../../assets/images/find.mp4';
+import freeVideo from '../../assets/images/free.mp4';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -13,13 +17,18 @@ import AnimatedPath from './components/AnimatedPath/AnimatedPath';
 
 import styles from './Home.module.css';
 
+const heroImageSrcSet = `${heroSmallImage} 500w, ${heroMediumImage} 1000w, ${heroLargeImage} 1500w, ${heroExtraLargeImage} 2000w`;
+
 const Home = () => {
   const wrapperRef = useRef<HTMLElement>(null);
 
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source type="image/webp" srcSet={heroImageSrcSet} />
+          <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -33,9 +42,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" imageSrc={trendingVideo} />
+            <FeatureItem title="Find gif for free" imageSrc={findVideo} />
+            <FeatureItem title="Free for everyone" imageSrc={freeVideo} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
