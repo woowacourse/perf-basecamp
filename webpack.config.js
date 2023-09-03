b60924faf972f5d872a2a818452b9021e64cb3c4
turtle601 <turtle601@naver.com>
2023-09-03 15:28:06 +0900

    chore: 소스코드 js파일 최적화

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+const mode = process.env.NODE_ENV || 'development';
+
+const isProd = mode === 'production';
 
 module.exports = {
+  mode,
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
@@ -16,16 +25,6 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './public', to: './public' }]
-    }),
-    new Dotenv()
-  ],
   module: {
     rules: [
       {
@@ -35,9 +34,24 @@ module.exports = {
           loader: 'ts-loader'
         }
       },
+      // style-loader, css-loader 구성
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        exclude: /\.module\.css$/i, // 모듈 파일 제외 설정
+        use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader']
+      },
+      // CSS Module ([filename].module.css)
+      {
+        test: /\.module\.css$/i,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -48,7 +62,30 @@ module.exports = {
       }
     ]
   },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true, // 빈칸 제거,
+        removeComments: true // 주석 제거
+      },
+      template: './index.html',
+      hash: true // 결과물 index.html이 매 빌드마다 다른 queryString과 함께 bundle.js를 불러들이도록 함으로써 파일이 동일하지 않다는 것을 알림 - 캐싱 방지
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './public', to: './public' }]
+    }),
+    new Dotenv(),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].[contenthash:8].css',
+      chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    })
+  ],
   optimization: {
-    minimize: false
+    minimize: true,
+    minimizer: isProd ? [new CssMinimizerPlugin(), new TerserPlugin()] : []
   }
 };
