1207eb20ee044e2f3c27d2274adad52fd5028011
Q Kim <qoli0703@gmail.com>
2023-09-03 18:05:19 +0900

    refactor: 검색 기능에 lazy 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+
+const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
@@ -14,7 +16,14 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search"
+          element={
+            <Suspense fallback={<p>로딩중..</p>}>
+              <Search />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
