a81d455816b1f398d2d61a7a9899b98bb99c686c
Patrick <kuyt3122@naver.com>
2023-09-04 02:16:33 +0900

    refactor: react lazy로 코드 스플리팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+const Search = lazy(() => import('./pages/Search/Search'));
+
+const SearchSuspense = () => {
+  return (
+    <Suspense fallback={<>Loading 중...</>}>
+      <Search />
+    </Suspense>
+  );
+};
+
 const App = () => {
   return (
-    <Router basename={'/perf-basecamp'}>
+    <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/search" element={<SearchSuspense />} />
       </Routes>
       <Footer />
     </Router>
