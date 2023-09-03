f8147809d0c12ae51800338b09e294ffffcd5206
sh981013s <sh981013s@gmail.com>
2023-09-04 02:58:40 +0900

    chore: chunk output contenthash 적용

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
