111cb0f047271c6298f38e3fe1cca9f1daa5f893
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-04 17:26:50 +0900

    refactor: Code Splitting을 위한 Suspense 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {lazy} from "react";
+import {lazy, Suspense} from "react";
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
@@ -12,12 +12,14 @@ const Search = lazy(() => import('./pages/Search/Search'));
 const App = () => {
   return (
     <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/search" element={<Search/>}/>
-      </Routes>
-      <Footer/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/search" element={<Search/>}/>
+        </Routes>
+        <Footer/>
+      </Suspense>
     </Router>
   );
 };
