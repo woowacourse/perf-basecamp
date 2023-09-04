f30bbb808b720231298141571729ddde32fe62f2
semnil5202 <semnil5202@gmail.com>
2023-09-04 14:34:57 +0900

    refactor: 메인페이지 이미지 크기 줄이기

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,9 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.webp';
+import heroImage_2400w from '../../assets/images/hero_2400w.webp';
+import heroImage_768w from '../../assets/images/hero_768w.webp';
+import heroImage_375w from '../../assets/images/hero_375w.webp';
 import trendingGif from '../../assets/images/trending.gif';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
@@ -19,7 +21,11 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source media="(max-width: 375px)" srcSet={heroImage_375w} />
+          <source media="(max-width: 768px)" srcSet={heroImage_768w} />
+          <img className={styles.heroImage} src={heroImage_2400w} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
