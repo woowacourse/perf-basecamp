599dd10bffc70077ccf8423c500778f0a145d91a
kyw0716 <kyw0716@naver.com>
2023-09-04 15:09:31 +0900

    chore: hero 이미지 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 const Home = React.lazy(() => import('./pages/Home/Home'));
 const Search = React.lazy(() => import('./pages/Search/Search'));
@@ -13,10 +13,12 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
