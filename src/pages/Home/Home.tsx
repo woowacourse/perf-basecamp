ebb03736b2acd54192c1a9edd425d87abe933dcf
Jungwoo <evencoding@gmail.com>
2023-09-04 22:15:05 +0900

    refactor: PNG 형식의 이미지를 webp, jpg 형식의 이미지로 압축 및 변환

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import heroImage from '../../assets/images/hero.webp';
+import heroImageJPG from '../../assets/images/hero.jpg';
 import trendingGif from '../../assets/images/trending.gif';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
@@ -19,7 +20,10 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source className={styles.heroImage} srcSet={heroImage} type="image/webp" />
+          <img className={styles.heroImage} src={heroImageJPG} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
