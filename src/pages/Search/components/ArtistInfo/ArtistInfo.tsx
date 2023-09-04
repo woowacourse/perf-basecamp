5555e51d17c089933b46e14685f2b0d58d015f38
Dain Lee <feb.dain@gmail.com>
2023-09-05 01:56:26 +0900

    refactor: 이미지 lazy loading 설정

--- src/pages/Search/components/ArtistInfo/ArtistInfo.tsx
+++ src/pages/Search/components/ArtistInfo/ArtistInfo.tsx
@@ -11,7 +11,7 @@ const ArtistInfo = ({ artist }: ArtistProps) => {
 
   return (
     <li className={styles.artistContainer}>
-      <img className={styles.profileImage} src={profileImageUrl} />
+      <img loading="lazy" className={styles.profileImage} src={profileImageUrl} />
       <p>
         <a className={styles.profileUrl} href={profileUrl}>
           {name}
