b675042d04dd7c1557d555de7080483e2c9bec7a
regularPark <srk99279@naver.com>
2023-09-04 19:37:29 +0900

    fix: `Suspense` 내부로 `Footer` 이동하여 `CLS` 향상

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
@@ -27,9 +26,6 @@ module.exports = {
       patterns: [{ from: './public', to: './public' }]
     }),
     new Dotenv(),
-    new CompressionPlugin({
-      exclude: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|txt|map|ico)$/i
-    }),
     new BundleAnalyzerPlugin()
   ],
   module: {
