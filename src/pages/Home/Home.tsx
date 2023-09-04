62fd2de450eb8eb2c20dbcd904349c54b3910081
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-04 17:54:40 +0900

    refactor: `hero` 이미지 최적화

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,9 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
+import heroDesktopImage from '../../assets/images/hero-1980.webp';
+import heroTabletImage from '../../assets/images/hero-768.webp';
+import heroMobileImage from '../../assets/images/hero-375.webp';
 import trendingGif from '../../assets/images/trending.gif';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
@@ -19,12 +21,24 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source
+            type='image/webp'
+            src={heroDesktopImage}
+            srcSet={`
+							${heroMobileImage} 375w,
+							${heroTabletImage} 768w,
+							${heroDesktopImage} 1980w,
+						`}
+          />
+          <img className={styles.heroImage} src={heroDesktopImage} alt='hero' />
+        </picture>
+        {/* <img className={styles.heroImage} src={heroImage} alt="hero image" /> */}
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
         </div>
-        <Link to="/search">
+        <Link to='/search'>
           <button className={classNames(styles.cta, styles.linkButton)}>start search</button>
         </Link>
       </section>
@@ -33,16 +47,16 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title='See trending gif' imageSrc={trendingGif} />
+            <FeatureItem title='Find gif for free' imageSrc={findGif} />
+            <FeatureItem title='Free for everyone' imageSrc={freeGif} />
           </div>
-          <Link to="/search">
+          <Link to='/search'>
             <button className={styles.linkButton}>start search</button>
           </Link>
         </div>
       </section>
-      <CustomCursor text="memegle" />
+      <CustomCursor text='memegle' />
     </>
   );
 };
