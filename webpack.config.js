548aa2fe9e08ecf59d691ef3c6a9f3e842780e02
Leejin-Yang <ejin1996@gmail.com>
2023-09-04 22:29:44 +0900

    refactor: css 파일 contenthash 추가

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     }),
     new Dotenv(),
     new BundleAnalyzerPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })
   ],
   module: {
     rules: [
