06ca9b06d1e1895672275c5fbd7b0ee07f15fddc
hozzijeong <ghwls0905@naver.com>
2023-09-04 12:30:25 +0900

    feat: 새로들어올 때마다 API 요청 방지

---
+++ src/utils/cache.ts
@@ -0,0 +1,25 @@
+type CacheValue = { time: number; result: unknown };
+
+const cache = new Map<string, CacheValue>();
+
+const CACHE_TIME = 30000;
+
+const checkCache = <T>(query: string, callback: () => T): T => {
+  const currentTime = new Date().getTime();
+  const cacheData = cache.get(query);
+
+  if (cacheData && cacheData.time < currentTime) {
+    return cacheData.result as T;
+  }
+
+  const data = callback();
+
+  cache.set(query, {
+    time: currentTime + CACHE_TIME,
+    result: data
+  });
+
+  return data;
+};
+
+export default checkCache;
