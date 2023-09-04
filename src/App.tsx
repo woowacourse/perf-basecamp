95bbfa1ccb20d128aab11792e1eb2bc9224561b8
sh981013s <sh981013s@gmail.com>
2023-09-05 02:02:49 +0900

    refactor: gihpy 모듈 home page 에서 불러오지 않도록 lazy import 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+const Search = lazy(() => import('./pages/Search/Search'));
+
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
