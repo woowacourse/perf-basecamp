9866d397ba7eb7518bbcabcbe79ac300c03c0edd
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-05 15:56:29 +0900

    feat: FeatureItem 컴포넌트에 gif를 webm, mp4로 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,10 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImageResponsive from '../../assets/images/hero.png';
+import trendingVideo from '../../assets/images/trending.webm';
+import findVideo from '../../assets/images/find.webm';
+import freeVideo from '../../assets/images/free.webm';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -19,7 +19,15 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <img
+          className={styles.heroImage}
+          src={heroImageResponsive.src}
+          srcSet={heroImageResponsive.srcSet}
+          width={heroImageResponsive.width}
+          height={heroImageResponsive.height}
+          loading="lazy"
+          alt="hero image"
+        />
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -33,9 +41,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" videoSrc={trendingVideo} />
+            <FeatureItem title="Find gif for free" videoSrc={findVideo} />
+            <FeatureItem title="Free for everyone" videoSrc={freeVideo} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -2,3 +2,7 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.gif';
 declare module '*.svg';
+declare module '*.webp';
+declare module '*.webm';
+
+declare module '*.responsive.png';
