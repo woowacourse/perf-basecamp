225ee6f03822f982fff61bacc0d83c0e1c99256f
gyeongza <gyeongza@naver.com>
2023-09-05 10:45:09 +0900

    refactor: 이미지 형식 webp -> avif로 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.webp';
+import heroImage from '../../assets/images/hero.avif';
 import trendingMp4 from '../../assets/images/trending.mp4';
 import findMp4 from '../../assets/images/find.mp4';
 import freeMp4 from '../../assets/images/free.mp4';
