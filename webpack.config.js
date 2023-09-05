8e01d68a3a1b6832c3f724f51436bac963332e04
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-05 13:25:08 +0900

    fix: webpack.config.js에 webm 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|webm)$/i,
                 loader: 'file-loader',
                 options: {
                     name: 'static/[name].[ext]'
