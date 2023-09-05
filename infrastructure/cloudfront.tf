e02720eeecca68e20a6cbca9773bca104310f8fc
solo5star <solo_5@naver.com>
2023-09-05 14:03:17 +0900

    build: 인프라 캐시 정책 추가 및 http3 지원

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -17,14 +17,27 @@ resource "aws_cloudfront_distribution" "default" {
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
+  http_version        = "http2and3"
 
   default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = aws_s3_bucket.main.id
-    compress               = true
-    cache_policy_id        = aws_cloudfront_cache_policy.default.id
-    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods            = ["GET", "HEAD"]
+    cached_methods             = ["GET", "HEAD"]
+    target_origin_id           = aws_s3_bucket.main.id
+    compress                   = true
+    cache_policy_id            = aws_cloudfront_cache_policy.assets.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.assets.id
+    viewer_protocol_policy     = "redirect-to-https"
+  }
+
+  ordered_cache_behavior {
+    path_pattern               = "/index.html"
+    allowed_methods            = ["GET", "HEAD"]
+    cached_methods             = ["GET", "HEAD"]
+    target_origin_id           = aws_s3_bucket.main.id
+    compress                   = true
+    cache_policy_id            = aws_cloudfront_cache_policy.html.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.html.id
+    viewer_protocol_policy     = "redirect-to-https"
   }
 
   viewer_certificate {
@@ -38,12 +51,12 @@ resource "aws_cloudfront_distribution" "default" {
   }
 }
 
-resource "aws_cloudfront_cache_policy" "default" {
-  name = "DefaultCachePolicy"
+resource "aws_cloudfront_cache_policy" "assets" {
+  name = "AssetsCachePolicy"
 
-  default_ttl = 1800
-  min_ttl     = 0
-  max_ttl     = 1800
+  default_ttl = 31536000 // 365d
+  min_ttl     = 31536000 // 365d
+  max_ttl     = 31536000 // 365d
 
   parameters_in_cache_key_and_forwarded_to_origin {
     enable_accept_encoding_brotli = true
@@ -57,6 +70,62 @@ resource "aws_cloudfront_cache_policy" "default" {
     }
     headers_config {
       header_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "html" {
+  name = "HtmlCachePolicy"
+
+  default_ttl = 1296000 // 15d
+  min_ttl     = 1296000 // 15d
+  max_ttl     = 2592000 // 30d
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_brotli = true
+    enable_accept_encoding_gzip   = true
+
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "assets" {
+  name    = "Assets"
+  comment = "Response Headers for assets (includes images, js, css)"
+
+  custom_headers_config {
+    items {
+      header   = "Cache-Control"
+      override = true
+      value = format(
+        "immutable, max-age=%d",
+        aws_cloudfront_cache_policy.assets.default_ttl
+      )
+    }
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "html" {
+  name    = "Html"
+  comment = "Response Headers for html"
+
+  custom_headers_config {
+    items {
+      header   = "Cache-Control"
+      override = true
+      // max-age=1h, stale-while-revalidate=15d
+      value = format(
+        "max-age=3600, stale-while-revalidate=%d",
+        aws_cloudfront_cache_policy.html.default_ttl
+      )
     }
   }
 }
