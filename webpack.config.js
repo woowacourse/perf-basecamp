63c1ca99d3914ff22bf8559c04c0d49f4e879040
n0eyes <doll4586633@naver.com>
2023-09-05 11:49:52 +0900

    chore: css 분리 및 압축
    - CssMinimizerWebpackPlugin
    - MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -24,7 +29,10 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
   ],
   module: {
     rules: [
@@ -37,7 +45,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -49,6 +57,8 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    splitChunks: { chunks: 'all' },
+    minimizer: [new CssMinimizerPlugin()]
   }
 };
