9a71d2b721bd19e736da528f0d883623ac7f8ff7
Ashley Heo <ashleysyheo@gmail.com>
2023-09-05 11:43:40 +0900

    refactor: Home lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
-// const Home = lazy(() => import('./pages/Home/Home'));
+const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 import './App.css';
-import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
       <Suspense fallback={<div>Loading!</div>}>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
         </Routes>
+        <Footer />
       </Suspense>
-      <Footer />
     </Router>
   );
 };
