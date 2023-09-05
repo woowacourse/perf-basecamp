4748b1ab0e2739944b70eb404e05a261e6735ece
yeopto <gunyeop530@gmail.com>
2023-09-05 15:33:28 +0900

    refactor: Home 컴포넌트 lazy load 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+const Home = React.lazy(() => import('./pages/Home/Home'));
 const Search = React.lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
-    // <Router>
-    <Router basename={'/perf-basecamp'}>
+    <Router>
+      {/* // <Router basename={'/perf-basecamp'}> */}
       <NavBar />
       <Suspense fallback={<div>로딩중</div>}>
         <Routes>
