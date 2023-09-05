0db7c9269357a828c7a3345ecad906c8c8066c76
woo-jk <201702042@o.cnu.ac.kr>
2023-09-05 16:14:56 +0900

    refactor: Home 컴포넌트 lazy import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
-const Home = lazy(() => import('./pages/Home/Home'));
+import Home from './pages/Home/Home';
 const Search = lazy(() => import('./pages/Search/Search'));
 
 import NavBar from './components/NavBar/NavBar';
