94c5e0e86544b718ce15aa457db02cbca95d4941
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-05 15:55:45 +0900

    feat: code splitting 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import React from 'react';
+
+const SearchPage = React.lazy(() => import('./pages/Search/Search'));
+const Home = React.lazy(() => import('./pages/Home/Home'));
+
+const SearchPageWithLazy = () => {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <SearchPage />
+    </React.Suspense>
+  );
+};
+
+const HomePageWithLazy = () => {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Home />
+    </React.Suspense>
+  );
+};
 
 const App = () => {
   return (
-    <Router basename={'/perf-basecamp'}>
+    <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<HomePageWithLazy />} />
+        <Route path="/search" element={<SearchPageWithLazy />} />
       </Routes>
       <Footer />
     </Router>
