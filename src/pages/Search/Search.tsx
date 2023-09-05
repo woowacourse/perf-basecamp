80b4c0742935ede914b43b9d104be2713a47f2a9
cruelladevil <dev.timetravel@gmail.com>
2023-09-06 01:11:39 +0900

    refactor: HelpPanel 컴포넌트 코드 스플리팅

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,11 +1,12 @@
-import { KeyboardEvent } from 'react';
+import { KeyboardEvent, Suspense, lazy } from 'react';
 import useGifSearch from './hooks/useGifSearch';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import SearchResult from './components/SearchResult/SearchResult';
-import HelpPanel from './components/HelpPanel/HelpPanel';
 
 import styles from './Search.module.css';
+
+const HelpPanel = lazy(() => import('./components/HelpPanel/HelpPanel'));
 
 const Search = () => {
   const { status, searchKeyword, gifList, searchByKeyword, updateSearchKeyword, loadMore } =
@@ -28,7 +29,9 @@ const Search = () => {
         onSearch={searchByKeyword}
       />
       <SearchResult status={status} gifList={gifList} loadMore={loadMore} />
-      <HelpPanel />
+      <Suspense fallback={null}>
+        <HelpPanel />
+      </Suspense>
     </div>
   );
 };
