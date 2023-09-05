aa4666856c66e524199785f2ecd408bfed246913
hafnium1923 <hafnium1923@gmail.com>
2023-09-05 11:22:46 +0900

    feat: css minify

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,7 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()]
-  }
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
+  },
+  plugins: [new MiniCssExtractPlugin()]
 };
