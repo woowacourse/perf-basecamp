e8ada067d699436ce8a1c173942c1d8b7f7fb19b
Ashley Heo <ashleysyheo@gmail.com>
2023-09-05 10:19:03 +0900

    refactor: 파일 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
+// const Home = lazy(() => import('./pages/Home/Home'));
+const Search = lazy(() => import('./pages/Search/Search'));
+
 import './App.css';
 import Home from './pages/Home/Home';
-
-const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
