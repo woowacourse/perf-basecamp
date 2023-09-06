b97875225a08a30c06bdf38981cecacc2ae35a78
inyeong-kang <tomatorkd@naver.com>
2023-09-06 18:11:53 +0900

    fix: lazy로 로딩되는 컴포넌트를 Suspense로 감싸 react-router 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import Home from './pages/Home/Home';
 
-const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
@@ -15,7 +15,14 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Search />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
