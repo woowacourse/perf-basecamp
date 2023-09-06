9a2a61505aac48e044d59f0c1fd0b32e773ca7c6
inyeong-kang <tomatorkd@naver.com>
2023-09-07 01:20:43 +0900

    chore: 불필요한 loading 속성 제거

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -8,7 +8,7 @@ type FeatureItemProps = {
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img loading="lazy" className={styles.featureImage} src={imageSrc} alt={'feature image'} />
+      <img className={styles.featureImage} src={imageSrc} alt={'feature image'} />
       <div className={styles.featureTitleBg}></div>
       <h3 className={styles.featureTitle}>{title}</h3>
     </div>
