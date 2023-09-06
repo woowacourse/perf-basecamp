3e494b164c23ab190baf7495b0a820b0d53bf098
jeongwusi <jeongwusi@gmail.com>
2023-09-06 13:22:56 +0900

    ♻️ refactor: trending gif 정보를 위한 캐시 로직 구현과 모듈화

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -9,19 +9,6 @@ const gf = new GiphyFetch(apiKey);
 const DEFAULT_FETCH_COUNT = 16;
 const TRENDING_GIF_API = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=${DEFAULT_FETCH_COUNT}&rating=g`;
 
-// 캐시 데이터를 저장할 객체
-const cache: Record<string, GifImageModel[]> = {};
-
-// 데이터를 캐시하는 유틸리티 함수
-function cacheData(key: string, data: GifImageModel[]): void {
-  cache[key] = data;
-}
-
-// 캐시된 데이터를 반환하는 유틸리티 함수
-function getCachedData(key: string): GifImageModel[] | null {
-  return cache[key] || null;
-}
-
 function convertResponseToModel(gifList: IGif[]): GifImageModel[] {
   return gifList.map((gif) => {
     const { id, title, images } = gif;
@@ -35,28 +22,26 @@ function convertResponseToModel(gifList: IGif[]): GifImageModel[] {
 }
 
 export const gifAPIService = {
+  /**
+   * treding gif 목록을 가져옵니다.
+   * @returns {Promise<GifImageModel[]>}
+   * @ref https://developers.giphy.com/docs/api/endpoint#!/gifs/trending
+   */
   getTrending: async function (): Promise<GifImageModel[]> {
     try {
-      // 캐시된 데이터를 확인
-      const cachedData = getCachedData('trending');
-
-      // 캐시된 데이터가 있으면 캐시된 데이터를 반환
-      if (cachedData !== null) {
-        return cachedData;
-      }
-
       const gifs: GifsResult = await fetch(TRENDING_GIF_API).then((res) => res.json());
-      const gifModels = convertResponseToModel(gifs.data);
-
-      // 데이터를 캐시
-      cacheData('trending', gifModels);
-
-      return gifModels;
+      return convertResponseToModel(gifs.data);
     } catch (e) {
       return [];
     }
   },
-
+  /**
+   * 검색어에 맞는 gif 목록을 가져옵니다.
+   * @param {string} keyword
+   * @param {number} page
+   * @returns {Promise<GifImageModel[]>}
+   * @ref https://developers.giphy.com/docs/api/endpoint#!/gifs/search
+   */
   searchByKeyword: async function (keyword: string, page: number): Promise<GifImageModel[]> {
     const searchOptions: SearchOptions = {
       limit: DEFAULT_FETCH_COUNT,
