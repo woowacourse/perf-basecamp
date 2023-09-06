2f39d410b4d0781a4c540a710b314a3adb12f069
Suyoung Jung <jsy19970512@naver.com>
2023-09-06 16:07:54 +0900

    refactor: hero 이미지 형식 변경 및 크기 축소

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,9 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
+import heroImageAvif from '../../assets/images/hero.avif';
+import heroImageWebp from '../../assets/images/hero.webp';
+import heroImage from '../../assets/images/hero.jpg';
 import freeGif from '../../assets/images/free.gif';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
@@ -19,7 +19,11 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source srcSet={heroImageAvif} type="image/avif" />
+          <source srcSet={heroImageWebp} type="image/webp" />
+          <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
