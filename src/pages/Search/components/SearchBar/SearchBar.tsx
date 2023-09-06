ee89801c0661bae130be0fafc5c2abdf72749552
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-06 15:09:10 +0900

    refactor: `@react-icons/all-files` 패키지 `react-icons` 로 수정

--- src/pages/Search/components/SearchBar/SearchBar.tsx
+++ src/pages/Search/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { AiOutlineSearch } from '@react-icons/all-files/ai/AiOutlineSearch';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 import styles from './SearchBar.module.css';
 
