c2da50a6bbf5812db619bdded11029f13939e885
jeongwusi <jeongwusi@gmail.com>
2023-09-06 13:19:04 +0900

    ♻️ refactor: 기존 확장자에서 webp와 webm으로 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,13 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
+import heroImgSmall from '../../assets/images/hero-s.webp';
+import heroImgMedium from '../../assets/images/hero-m.webp';
+import heroImgLarge from '../../assets/images/hero-l.webp';
 import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import trendingGif from '../../assets/images/trending.webm';
+import findGif from '../../assets/images/find.webm';
+import freeGif from '../../assets/images/free.webm';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -19,7 +22,12 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source srcSet={heroImgSmall} type="image/webp" media="(max-width: 480px)" />
+          <source srcSet={heroImgMedium} type="image/webp" media="(max-width: 1024px)" />
+          <source srcSet={heroImgLarge} type="image/webp" media="(max-width: 1680px)" />
+          <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -33,9 +41,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" videoSrc={trendingGif} />
+            <FeatureItem title="Find gif for free" videoSrc={findGif} />
+            <FeatureItem title="Free for everyone" videoSrc={freeGif} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,5 +1,5 @@
 declare module '*.png';
 declare module '*.jpg';
-declare module '*.gif';
 declare module '*.svg';
-declare module '*.esm';
+declare module '*.webp';
+declare module '*.webm';
