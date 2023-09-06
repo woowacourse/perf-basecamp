f8853ba52a613be1079cc2ee41afabbe667a914a
jeongwusi <jeongwusi@gmail.com>
2023-09-06 13:17:49 +0900

    ♻️ refactor: @react-icons/all-files 라이브러리를 통한 트리 쉐이킹 적용

--- src/pages/Search/components/SearchBar/SearchBar.tsx
+++ src/pages/Search/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiOutlineSearch } from 'react-icons/ai/index.esm';
+import { AiOutlineSearch } from '@react-icons/all-files/ai/AiOutlineSearch';
 
 import styles from './SearchBar.module.css';
 
