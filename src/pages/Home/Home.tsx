5a35a3d3b9fb40ba9b75f4cce82d7f8721de926b
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-09-07 15:46:14 +0900

    refactor: FeatureItem imageSrc -> videoSrc

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,10 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImage from '../../assets/images/hero.webp';
+import trendingGif from '../../assets/images/trending.mp4';
+import findGif from '../../assets/images/find.mp4';
+import freeGif from '../../assets/images/free.mp4';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -33,9 +33,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" videoSrc={trendingGif} />
+            <FeatureItem title="Find gif for free" videoSrc={findGif} />
+            <FeatureItem title="Free for everyone" videoSrc={freeGif} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
