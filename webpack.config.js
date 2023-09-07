368508dc1b9caca973bb73804787ecf0eab62b44
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-09-07 16:46:37 +0900

    refactor: webpack 번들링 최적화

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
+    assetModuleFilename: 'images/[hash][ext][query]',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -16,13 +25,21 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
     }),
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
+    }),
+    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css'
     }),
     new Dotenv()
   ],
@@ -37,18 +54,58 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
-        }
+        test: /\.(webm|webp|mp4)$/i,
+        type: 'asset/resource'
       }
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    usedExports: true,
+    splitChunks: {
+      cacheGroups: {
+        giphy: {
+          test: /[\\/]node_modules[\\/](@giphy)[\\/]/,
+          name: 'giphy',
+          enforce: true,
+          chunks: 'all'
+        },
+        reactIcons: {
+          test: /[\\/]node_modules[\\/](react-icons)[\\/]/,
+          name: 'react-icons',
+          chunks: 'all'
+        }
+      }
+    },
+    minimizer: [
+      '...',
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          },
+          format: {
+            comments: false
+          },
+          extractComments: false
+        }
+      }),
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminGenerate,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true, colors: 60, optimizationLevel: 3 }],
+              ['webp', { quality: 35, resize: { width: 1920, height: 0 } }]
+            ]
+          }
+        },
+        loader: false
+      })
+    ]
   }
 };
