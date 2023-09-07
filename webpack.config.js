30dc6d590e2e825a56697597dda406d74c71e218
bassyu <yrkdgus@gmail.com>
2023-09-07 15:29:40 +0900

    chore: contenthash 적용

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: '[name].[hash].js',
+    filename: '[name].[contenthash:8].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
