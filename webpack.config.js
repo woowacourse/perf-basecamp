bd2d4d2ba18d3a2dc58a2d97b35e92d47e20d5c6
nlom0218 <nlom0218@naver.com>
2023-09-08 15:05:35 +0900

    refactor: optimization 옵션 제거

--- webpack.config.js
+++ webpack.config.js
@@ -50,17 +50,5 @@ module.exports = {
         }
       }
     ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true
-          }
-        }
-      })
-    ]
   }
 };
