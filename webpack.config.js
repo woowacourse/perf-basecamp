7dd5bb1912859218b4ec71ffce8b86db21f01b6f
sᴏʟʙɪ ☔️ <solbi2004@naver.com>
2023-09-08 22:49:55 +0900

    [개인 미션 - 성능 오답노트] 해온(백솔비) 미션 제출합니다. (#69)
    * chore: npm 설치
    
    * chore: basename 삭제
    
    * chore: hash 값 추가
    
    * chore: minimize와 source map 설정
    
    * feat: 이미지 webp 변환 및 압축
    
    * chore: gif 파일 mp4로 변환
    
    * feat: 이미지 리사이징 구현
    
    * chore: mp4 추가
    
    * feat: gif를 mp4로 변환 구현
    
    * chore: 빌드시 이미지 static 파일 내부로 들어가도록 설정
    
    * feat: CSS minimizer 추가
    
    * feat: lazy 구현
    
    * chore: chunk file 이름 부여 설정
    
    * chore: 필요없는 속성 삭제
    
    * feat: preload 추가
    
    * feat: webpack-bundle-analyzer 추가
    
    * chore: devtool 삭제
    
    * feat: 캐시 스토리지를 통해 매번 요청하지 않도록 구현
    
    * refactor: translate로 변경
    
    * chore: 필요없는 주석 제거
    
    * feat: esbuild-loader 사용

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -16,39 +19,53 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      hash: true
     }),
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/i,
         exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
-        }
+        loader: 'esbuild-loader'
       },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
+        test: /\.(eot|svg|ttf|webp|woff|woff2|png|jpg|gif|mp4)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'static/[hash][ext][query]'
         }
       }
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        deleteOriginalAssets: false,
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminGenerate,
+          options: {
+            plugins: [
+              ['webp', { preset: 'photo', quality: 40, resize: { width: 1920, height: 1280 } }]
+            ]
+          }
+        }
+      })
+    ]
   }
 };
