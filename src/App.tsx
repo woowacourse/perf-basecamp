e07fa4c448db4246499314c4359ad08596dcc886
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-09 18:36:35 +0900

    feat: cache delete 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,26 @@ import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
+import { TRENDING_GIF_API } from './apis/gifAPIService';
 
 const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
+  useEffect(() => {
+    const clearCacheStorage = async () => {
+      const cache = await caches.open('trending');
+      cache.delete(TRENDING_GIF_API);
+    };
+
+    window.addEventListener('beforeunload', clearCacheStorage);
+
+    return () => {
+      window.removeEventListener('beforeunload', clearCacheStorage);
+    };
+  }, []);
+
   return (
     <Router>
       <Suspense fallback={<div>Loading ...</div>}>
