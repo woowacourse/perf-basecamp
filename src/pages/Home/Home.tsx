fd430d79d9773d8cf77408cc640eff7420ceb395
d0dam <smallkdb@gmail.com>
2023-09-11 07:08:22 +0900

    refactor: 불필요한 태그 삭제

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -19,10 +19,8 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <picture>
-          <source type="image/webp" className={styles.heroImage} />
-          <img className={styles.heroImage} src={heroImage} alt="hero" />
-        </picture>
+        <img className={styles.heroImage} src={heroImage} alt="hero" />
+
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
