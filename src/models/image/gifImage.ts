5146602c00576bd9262a62e28c66d186117c1e70
cruelladevil <dev.timetravel@gmail.com>
2023-09-10 18:36:07 +0900

    fix: GIPHY로부터 받아오는 mp4가 제대로 로드되지 않는 현상 수정
    - poster 다시 추가
    - Related to d082c2bba1158a2b5182e3f19c35ce9a23316275

--- src/models/image/gifImage.ts
+++ src/models/image/gifImage.ts
@@ -2,5 +2,6 @@ export interface GifImageModel {
   id: string | number;
   title: string;
   url: string;
+  imageUrl: string;
   videoUrl: string;
 }
--- src/pages/Search/components/GifItem/GifItem.tsx
+++ src/pages/Search/components/GifItem/GifItem.tsx
@@ -5,11 +5,19 @@ import styles from './GifItem.module.css';
 
 type GifItemProps = Omit<GifImageModel, 'id'>;
 
-const GifItem = ({ videoUrl = '', title = '', url = '' }: GifItemProps) => {
+const GifItem = ({ imageUrl = '', videoUrl = '', title = '', url = '' }: GifItemProps) => {
   return (
     <div className={styles.gifItem}>
       <a href={url}>
-        <video className={styles.gifImage} src={videoUrl} autoPlay muted loop playsInline />
+        <video
+          className={styles.gifImage}
+          src={videoUrl}
+          poster={imageUrl}
+          autoPlay
+          muted
+          loop
+          playsInline
+        />
         <div className={styles.gifTitleContainer}>
           <div className={styles.gifTitleBg}></div>
           <h4 className={styles.gifTitle}>{title}</h4>
