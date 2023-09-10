80ea1f1af5e700ff3a9ffe32a5ddc42fcda5e759
Q Kim <qoli0703@gmail.com>
2023-09-10 11:04:57 +0900

    feat: 로딩 스켈레톤 적용

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -6,7 +6,7 @@ import { GifImageModel } from '../models/image/gifImage';
 const apiKey = process.env.GIPHY_API_KEY || '';
 const gf = new GiphyFetch(apiKey);
 
-const DEFAULT_FETCH_COUNT = 16;
+export const DEFAULT_FETCH_COUNT = 16;
 const TRENDING_GIF_API = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=${DEFAULT_FETCH_COUNT}&rating=g`;
 
 let trendingCache: GifImageModel[]= [];
