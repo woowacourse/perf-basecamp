249ff22dfcdac8442fe0185d695be174f1b4de83
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-09-11 14:23:35 +0900

    [개인 미션 - 성능 오답노트] 네이브(윤지영) 미션 제출합니다.  (#87)
    * chore: MiniCssExtractPlugin,TerserPlugin,CssMinimizerPlugin적용
    
    * chore: 이미지 최적화 플러그인 설치
    
    * chore: image-webpack-loader적용
    
    * chore: 정적이미지 resizing & format변경
    
    * refactor: 최적화된 이미지 & 짤 적용
    
    * chore: 확장자 타입선언
    
    * chore: 폰트 preload적용
    
    * chore: webpack-bundle-analyzer 플러그인 적용
    
    * chore: webpack-bundle-analyzer 플러그인 적용
    
    * refactor: Home & Search 페이지에 lazy 적용
    
    * feat: 데이터 캐시 유틸함수 생성
    
    * refactor: trending api 캐싱로직 적용
    
    * design: layout shift 해결
    
    * refactor: 컴포넌트에 memo적용
    
    * fix: 수정사항 반영
    
    * fix: TerserPlugin 배열 제거
    
    * refactor: 불필요한 플러그인 제거

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,4 +1,7 @@
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.gif';
+declare module '*.mp4';
 declare module '*.svg';
+declare module '*.webp';
+declare module '*.avif';
--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,73 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/dist'),
-    clean: true
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true
-  },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './public', to: './public' }]
-    }),
-    new Dotenv()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+    output: {
+      filename: 'bundle.[chunkhash].js',
+      path: path.join(__dirname, '/dist'),
+      clean: true
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true
+    },
+    devtool: 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './public', to: './public' }]
+      }),
+      new Dotenv(),
+      new MiniCssExtractPlugin(),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        generateStatsFile: true,
+        statsFilename: 'bundle-report.json'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader'
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: [isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|mp4|avif)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'static/[name].[ext]'
+          }
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|webp|mp4)$/i,
+          loader: 'image-webpack-loader',
+          enforce: 'pre'
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: false
-  }
+      ]
+    },
+    optimization: {
+      splitChunks: { chunks: 'all' },
+      minimizer: [`...`, new CssMinimizerPlugin()]
+    }
+  };
 };
