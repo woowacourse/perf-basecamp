c9f0eb9a815baf1f2005470361f311f61383c1be
Geonyeop Kim <gunyeop530@gmail.com>
2023-09-11 15:34:03 +0900

    [개인 미션 - 성능 오답노트] 엽토(김건엽) 미션 제출합니다. (#78)
    * chore: 기본 설정
    
    * build: 번들 크기 줄이기 위한 terser plugin 적용
    
    * refactor: webp, mp4로 이미지 포매팅 변경 및 변경에 따른 설정
    
    * refactor: Search 컴포넌트 lazy load
    
    * refactor: Home 컴포넌트 lazy load 설정
    
    * build: bundle-analyzer 플러그인 추가
    
    * feat: trendingAPI를 매번 요청하지 않도록 cache를 설정
    
    * refactor: 추가 로드시 추가된 목록만 새로 렌더되도록 변경
    
    * refactor: 필요한 react-icons 만 빌드되도록 변경
    
    * build: dev 모드일때만 source map 제공하도록 변경
    
    * refactor: Layout Shift, Frame Drop이 일어나지 않도록 변경하다.
    
    * refactor: Suspense 위치 변경
    
    * refactor: hero 이미지 avif 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+const Home = React.lazy(() => import('./pages/Home/Home'));
+const Search = React.lazy(() => import('./pages/Search/Search'));
+
 const App = () => {
   return (
-    <Router basename={'/perf-basecamp'}>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Suspense fallback={<div>로딩중</div>}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const isDevMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[contenthash].bundle.js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -16,7 +20,7 @@ module.exports = {
     open: true,
     historyApiFallback: true
   },
-  devtool: 'source-map',
+  devtool: isDevMode ? 'source-map' : false,
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
@@ -24,7 +28,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin()
   ],
   module: {
     rules: [
@@ -40,7 +45,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|mp4|avif)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
@@ -49,6 +54,15 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
+    ]
   }
 };
