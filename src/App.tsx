3238337149c68338f39ca4d1d1eab44038d7fe1d
semnil5202 <semnil5202@gmail.com>
2023-09-11 15:27:55 +0900

    refactor: Home 컴포넌트에 lazy 로딩 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
@@ -9,23 +7,20 @@ import './App.css';
 import { Suspense, lazy } from 'react';
 
 const Search = lazy(() => import('./pages/Search/Search'));
-
-const SearchPageWithSuspense = () => (
-  <Suspense fallback={<div>loading...</div>}>
-    <Search />
-  </Suspense>
-);
+const Home = lazy(() => import('./pages/Home/Home'));
 
 const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchPageWithSuspense />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Suspense fallback={<div>loading...</div>}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
