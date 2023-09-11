a6b273c21431e856b1efff226583da79e367a082
Dahye Yun <102305630+Dahyeeee@users.noreply.github.com>
2023-09-11 13:14:43 +0900

    [개인 미션 - 성능 오답노트] 헤다(윤다혜) 미션 제출합니다. (#96)
    * init project
    
    * chore:번들 사이즈 줄이기
    
    * chore: assets 이미지,gif 최적화 업로드
    
    * refactor: 리플로우 일어나지 않도록 css 수정
    
    * refactor: asset 파일확장자 변환 최적화
    
    * refactor:사용하는 아이콘만 불러오도록 수정
    
    * refactor: 랜더링 최적화
    
    * trending gif 캐싱
    
    * suspense범위 확장
    
    * chore:ttl 값 수정, 리드미 수정

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -2,13 +2,16 @@ import styles from './FeatureItem.module.css';
 
 type FeatureItemProps = {
   title: string;
-  imageSrc: string;
+  imageSrc: { gif: string; webp: string };
 };
 
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      <picture>
+        <source className={styles.featureImage} srcSet={imageSrc.webp} type="image/webp" />
+        <img className={styles.featureImage} src={imageSrc.gif} />
+      </picture>
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
