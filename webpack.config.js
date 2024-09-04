ec53f6358c208f491344f11869671204ea8dc101
skiende74 <skiende74@gmail.com>
2024-09-04 15:55:09 +0900

    chore: package.json 의존성 최신화 및 imagemin 활용하여 이미지 압축

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const os = require('os');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -43,14 +44,30 @@ module.exports = {
         }
       },
       {
+        // 일반적인 CSS 파일을 처리하기 위한 규칙
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        exclude: /\.module\.css$/i, // CSS Modules 파일 제외
+        use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        // CSS Modules 파일을 처리하기 위한 규칙
+        test: /\.module\.css$/i,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // 개발 모드에서는 'style-loader', 프로덕션 모드에서는 'MiniCssExtractPlugin.loader'
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              esModule: false
+            }
+          }
+        ]
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/[name].[ext]'
         }
       }
     ]
@@ -58,13 +75,27 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [
+      // '...',
       new CssMinimizerPlugin({
         // CPU 멀티 프로세서 병렬화 옵션 (기본 값: true)
-        parallel: os.cpus().length - 1
+        parallel: true
       }),
       new TerserPlugin({
         terserOptions: {
           compress: {}
+        }
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              'imagemin-gifsicle',
+              'imagemin-jpegtran',
+              'imagemin-pngquant',
+              'imagemin-svgo'
+            ]
+          }
         }
       })
     ]
