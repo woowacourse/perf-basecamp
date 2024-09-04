571423ee611f6a89afa99f90d3b85a1475c28193
월하 <cerezo00@naver.com>
2024-09-04 17:45:38 +0900

    perf: convert png/gif to webp/webm

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -6,9 +6,18 @@ type FeatureItemProps = {
 };
 
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
+  const isWebM = imageSrc.endsWith('.webm');
+
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      {isWebM ? (
+        <video className={styles.featureImage} autoPlay loop muted>
+          <source src={imageSrc} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        <img className={styles.featureImage} src={imageSrc} alt={title} />
+      )}
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
