dfa9ad77ec6e4d866e842011500da8ee5a24a915
김진호 <rlawlsgh1227@gmail.com>
2024-09-04 13:25:37 +0900

    feat: 소스코드 용량 줄이기

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+/* eslint-disable prettier/prettier */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -24,7 +28,13 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['dist/*']
+    }), // 이전 빌드결과 지우기 위한 플러그인
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    })
   ],
   module: {
     rules: [
@@ -37,7 +47,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -49,6 +59,18 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true
+          },
+          format: {
+            comments: false
+          }
+        }
+      })
+    ]
   }
 };
