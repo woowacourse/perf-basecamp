ed2cddf3fe1fc95604acbc2fb0e1796cd2dc6e6e
jinsil <jinsil378900@gmail.com>
2024-09-04 17:58:19 +0900

    feat: css minify webpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -37,7 +39,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -49,6 +51,6 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimizer: [`...`, new CssMinimizerPlugin()]
   }
 };
