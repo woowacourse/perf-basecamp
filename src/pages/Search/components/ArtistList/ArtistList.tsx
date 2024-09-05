68e2ab5bab964a10ea29bbfea41840338be3faa5
ss0526100 <ss0526100@gmail.com>
2024-09-05 19:55:04 +0900

    feat(ArtistList): 메모이제이션 적용

--- src/pages/Search/components/ArtistList/ArtistList.tsx
+++ src/pages/Search/components/ArtistList/ArtistList.tsx
@@ -1,5 +1,6 @@
 import { Artist } from '../../../../models/help/artist';
 import ArtistInfo from '../ArtistInfo/ArtistInfo';
+import { memo } from 'react';
 
 type ArtistListProps = {
   artists: Artist[];
@@ -16,3 +17,13 @@ const ArtistList = ({ artists }: ArtistListProps) => {
 };
 
 export default ArtistList;
+export const MemoizedArtistList = memo(ArtistList, (prevArtists, nextArtists) =>
+  nextArtists.artists.every((nextArtist, index) => {
+    const prevArtist = prevArtists.artists[index];
+    return (
+      nextArtist.name === prevArtist.name &&
+      nextArtist.profileImageUrl === nextArtist.profileImageUrl &&
+      nextArtist.profileUrl === prevArtist.profileUrl
+    );
+  })
+);
