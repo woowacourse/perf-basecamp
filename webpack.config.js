45bd2c6a3337abcdc28c1497558ab8b5a623e035
novice0840 <magnolia0840@gmail.com>
2024-09-05 21:56:32 +0900

    feat: image 압축

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.tsx',
@@ -31,8 +31,8 @@ module.exports = {
     }),
     new Dotenv(),
     new MiniCssExtractPlugin(),
-    new CompressionPlugin()
-    // new BundleAnalyzerPlugin()
+    new CompressionPlugin(),
+    new BundleAnalyzerPlugin()
   ],
   module: {
     rules: [
