863e5d5991f495a2fab8bf27418fc1497e69d22e
Kim Da Eun <midekuna@gmail.com>
2024-09-05 17:28:44 +0900

    feat: webp 압축 설정 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-
-const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-
-const isDevMode = process.env.NODE_ENV === 'development';
-
-const commonPlugins = [
-  new HtmlWebpackPlugin({
-    template: './index.html'
-  }),
-  new CopyWebpackPlugin({
-    patterns: [
-      {
-        from: './public',
-        to: './public',
-        globOptions: {
-          ignore: ['**/404.html'] // 404.html 파일 제외
-        }
-      },
-      {
-        from: path.resolve(__dirname, './public/404.html'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]
-  }),
-  new webpack.EnvironmentPlugin({
-    NODE_ENV: process.env.NODE_ENV || 'development'
-  }),
-  new Dotenv(),
-  new BundleAnalyzerPlugin({
-    analyzerMode: 'static',
-    reportFilename: 'bundle-report.html',
-    openAnalyzer: true,
-    excludeAssets: [/node_modules/]
-  })
-];
-
-const devPlugins = [
-  // new BundleAnalyzerPlugin({
-  //   analyzerMode: 'static',
-  //   reportFilename: 'bundle-report.html',
-  //   openAnalyzer: true,
-  //   excludeAssets: [/node_modules/]
-  // })
-];
-
-const prodPlugins = [
-  new MiniCssExtractPlugin({
-    filename: '[name].[contenthash].css',
-    chunkFilename: '[id].[contenthash].css' // 코드 스플리팅 시 생성되는 추가적인 청크 파일의 이름
-  }),
-  new CompressionPlugin({
-    algorithm: 'gzip',
-    test: /\.(js|css|html|svg)$/,
-    threshold: 10240,
-    minRatio: 0.8
-  })
-];
-
-// 최종 플러그인 설정
-const plugins = [...commonPlugins, ...(isDevMode ? devPlugins : prodPlugins)];
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -74,17 +10,10 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   output: {
-    filename: isDevMode ? 'bundle.js' : 'bundle.[contenthash].js',
+    filename: 'bundle.js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
-  cache: false,
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true
-  },
-  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -96,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -107,8 +36,28 @@ module.exports = {
       }
     ]
   },
-  plugins,
-  optimization: {
-    minimizer: ['...', new CssMinimizerPlugin()]
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './public',
+          to: './public',
+          globOptions: {
+            ignore: ['**/404.html']
+          }
+        },
+        {
+          from: path.resolve(__dirname, './public/404.html'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: process.env.NODE_ENV || 'development'
+    }),
+    new Dotenv()
+  ]
 };
---
+++ webpack.prod.js
@@ -0,0 +1,65 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.js');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'bundle.[contenthash].js' // 해시를 추가하여 캐싱 전략 개선
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
+    }),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle-report.html',
+      openAnalyzer: false,
+      excludeAssets: [/node_modules/]
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.sharpMinify,
+          options: {
+            encodeOptions: {
+              webp: { quality: 80 },
+              png: { quality: 80 },
+              gif: { quality: 80 }
+            }
+          }
+        },
+        generator: [
+          {
+            preset: 'webp',
+            implementation: ImageMinimizerPlugin.sharpGenerate,
+            options: {
+              encodeOptions: {
+                webp: { quality: 80 }
+              }
+            }
+          }
+        ]
+      })
+    ]
+  }
+});
