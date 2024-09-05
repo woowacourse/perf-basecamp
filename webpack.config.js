7cae991e29f37ca6d049b29db808fe46784d3855
jinsil <jinsil378900@gmail.com>
2024-09-05 14:32:11 +0900

    feat: webm과 mp4 사용하여 gif 용량 줄이기

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|webm|mp4)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
