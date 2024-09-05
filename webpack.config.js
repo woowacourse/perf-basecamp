6e5e0ae1ac99def759a22255b49f498da16b3dab
badahertz52 <badahertz52@gmail.com>
2024-09-05 15:31:55 +0900

    ci : splitChunks 옵션 추가

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,7 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [new CssMinimizerPlugin()],
-    usedExports: true
+    usedExports: true,
+    splitChunks: { chunks: 'all' }
   }
 };
