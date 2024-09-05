cd929ed52bf3ab5b130576a6e5553909e3ace7b2
jinsil <jinsil378900@gmail.com>
2024-09-05 14:46:31 +0900

    feat: code splitting 하여 최적화하기

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,9 @@ module.exports = {
     ]
   },
   optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()]
+    minimizer: [`...`, new CssMinimizerPlugin()],
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 };
