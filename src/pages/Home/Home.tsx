fb3ed1e6247e29027ce216d0f135fbb0e54a2533
greetings1012 <ahnqwerty@gmail.com>
2024-09-05 17:41:52 +0900

    refactor: 이미지 파일 압축 및 형식 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,11 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImageWebp from '../../assets/images/hero.webp';
+import heroImageJpg from '../../assets/images/hero.jpg';
+import trendingMp4 from '../../assets/images/trending.mp4';
+import findMp4 from '../../assets/images/find.mp4';
+import freeMp4 from '../../assets/images/free.mp4';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -21,7 +22,16 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source srcSet={heroImageWebp} type="image/webp" />
+          <source srcSet={heroImageJpg} type="image/jpeg" />
+          <img
+            className={styles.heroImage}
+            src={heroImageJpg}
+            alt="hero image"
+            fetchPriority="high"
+          />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -35,9 +45,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" imageSrc={trendingMp4} />
+            <FeatureItem title="Find gif for free" imageSrc={findMp4} />
+            <FeatureItem title="Free for everyone" imageSrc={freeMp4} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
