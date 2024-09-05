a898429604920f365ad54320b8e5b47a1457f503
brgndy <brgndy96@gmail.com>
2024-09-05 20:08:36 +0900

    refactor: will-change 및 transform 으로 개선

--- src/pages/Home/components/CustomCursor/CustomCursor.tsx
+++ src/pages/Home/components/CustomCursor/CustomCursor.tsx
@@ -11,13 +11,29 @@ const CustomCursor = ({ text = '' }: CustomCursorProps) => {
   const [...cursorTextChars] = text;
   const mousePosition = useMousePosition();
   const cursorRef = useRef<HTMLDivElement>(null);
+  const mousePositionRef = useRef(mousePosition);
+  const animationFrameIdRef = useRef<number | null>(null);
 
   useEffect(() => {
-    if (cursorRef.current) {
-      cursorRef.current.style.top = `${mousePosition.pageY}px`;
-      cursorRef.current.style.left = `${mousePosition.pageX}px`;
-    }
+    mousePositionRef.current = mousePosition;
   }, [mousePosition]);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      if (cursorRef.current) {
+        cursorRef.current.style.transform = `translate(${mousePositionRef.current.pageX}px, ${mousePositionRef.current.pageY}px)`;
+      }
+      animationFrameIdRef.current = requestAnimationFrame(updatePosition);
+    };
+
+    animationFrameIdRef.current = requestAnimationFrame(updatePosition);
+
+    return () => {
+      if (animationFrameIdRef.current !== null) {
+        cancelAnimationFrame(animationFrameIdRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div ref={cursorRef} className={styles.cursor}>
