f6c03418822628977c6108cf97c9ae4f3e0f4599
Lee sang Yeop <mma7710@naver.com>
2024-09-05 13:04:29 +0900

    feat: search 페이지를 동적 lazy loading 하도록 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import { lazy } from 'react';
+
+const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
-    <Router basename={'/perf-basecamp'}>
+    <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
