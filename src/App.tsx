bdea95883f00196ab6f41829fef9a7f430581146
손진영 <sm099@naver.com>
2024-09-05 19:17:57 +0900

    refactor(App): Home lazy loading 제거 및 basename 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 
 import './App.css';
 
-const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const Loading = () => <div>Loading...</div>;
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={'/'}>
       <NavBar />
       <Suspense fallback={<Loading />}>
         <Routes>
