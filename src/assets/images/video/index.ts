1e03c7e48697683c7afd215d6cb6d26b9f88834a
김진호 <rlawlsgh1227@gmail.com>
2024-09-05 13:03:38 +0900

    feat: gif -> mp4로 이미지 용량 최적화 구현

---
+++ src/assets/images/video/index.ts
@@ -0,0 +1,3 @@
+export { default as Find } from './find.mp4';
+export { default as Free } from './free.mp4';
+export { default as Trending } from './trending.mp4';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import { HeroOriginal, HeroDesktop, HeroMobile } from '../../assets/images/hero';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import { Trending, Find, Free } from '../../assets/images/video';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -44,9 +42,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" imageSrc={Trending} />
+            <FeatureItem title="Find gif for free" imageSrc={Find} />
+            <FeatureItem title="Free for everyone" imageSrc={Free} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
