32537f51a0a7a06b643a434d1f97f2db809ee1b7
greetings1012 <ahnqwerty@gmail.com>
2024-09-05 20:06:21 +0900

    refactor: 캐시 스토리지를 이용해 검색 결과 및 trending gif 로드가 반복적으로 일어나지 않도록 수정

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -43,14 +43,34 @@ export const gifAPIService = {
    * @returns {Promise<GifImageModel[]>}
    * @ref https://developers.giphy.com/docs/api/endpoint#!/gifs/trending
    */
-  getTrending: async (): Promise<GifImageModel[]> => {
-    const url = apiClient.appendSearchParams(new URL(`${BASE_URL}/trending`), {
-      api_key: API_KEY,
-      limit: `${DEFAULT_FETCH_COUNT}`,
-      rating: 'g'
-    });
+  getTrending: async function (): Promise<GifImageModel[]> {
+    try {
+      const cacheStorage = await caches.open('trending');
+      const cachedResponse = await cacheStorage.match(`${BASE_URL}/trending`);
+      const url = apiClient.appendSearchParams(new URL(`${BASE_URL}/trending`), {
+        api_key: API_KEY,
+        limit: `${DEFAULT_FETCH_COUNT}`,
+        rating: 'g'
+      });
 
-    return fetchGifs(url);
+      if (cachedResponse) {
+        const gifs: GifsResult = await cachedResponse.json();
+        return convertResponseToModel(gifs.data);
+      }
+
+      const response = await fetch(url);
+
+      if (response.ok) {
+        await cacheStorage.put(url, response.clone());
+
+        const gifs: GifsResult = await response.json();
+        return convertResponseToModel(gifs.data);
+      } else {
+        throw new Error('Failed to fetch.');
+      }
+    } catch (error) {
+      return [];
+    }
   },
   /**
    * 검색어에 맞는 gif 목록을 가져옵니다.
