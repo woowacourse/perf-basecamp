c937a9c39944cb8fa8f1cbe97195da2905a046d7
simhorim <hori5020@gmail.com>
2024-09-06 11:08:40 +0900

    refactor(Home): hero img webp로 수정하여 이미지 크기 개선

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
         use: [isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
