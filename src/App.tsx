2f60151ce28c4e81c2b4bd3939738702f89e833e
Seongjin Hong <hongseongjin.to@gmail.com>
2024-09-06 16:27:11 +0900

    refactor: React.lazy를 통해 각 페이지 간 코드 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
+import NavBar from '@components/NavBar/NavBar';
+import Footer from '@components/Footer/Footer';
 
 import './App.css';
+
+const Home = lazy(async () => await import('@pages/Home/Home'));
+const Search = lazy(async () => await import('@pages/Search/Search'));
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<div>로딩중이에용</div>}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Suspense>
     </Router>
   );
 };
