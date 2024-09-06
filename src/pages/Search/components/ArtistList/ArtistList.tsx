1e09f1fc84bf515e8d0dd11394917dc62e1134fe
jaeml06 <jaeml0630@gmail.com>
2024-09-06 16:34:41 +0900

    feat: 선택 요구 사항 react-window를 사용하여 구현

--- src/pages/Search/components/ArtistList/ArtistList.tsx
+++ src/pages/Search/components/ArtistList/ArtistList.tsx
@@ -1,17 +1,24 @@
 import { Artist } from '../../../../models/help/artist';
 import ArtistInfo from '../ArtistInfo/ArtistInfo';
-
+import { FixedSizeList as List } from 'react-window';
 type ArtistListProps = {
   artists: Artist[];
 };
 
 const ArtistList = ({ artists }: ArtistListProps) => {
   return (
-    <ul>
-      {artists.map((artist, index) => {
-        return <ArtistInfo key={index} artist={artist} />;
-      })}
-    </ul>
+    <List
+      height={480} // 표시할 리스트의 전체 높이
+      itemCount={artists.length} // 리스트 아이템 수
+      itemSize={60} // 각 아이템의 고정 높이
+      width="100%" // 리스트 너비
+    >
+      {({ index, style }) => (
+        <div style={style}>
+          <ArtistInfo key={index} artist={artists[index]} />
+        </div>
+      )}
+    </List>
   );
 };
 
