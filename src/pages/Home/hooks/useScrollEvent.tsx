0edde861e1755b89244e8aa888c7279b4a2d4079
badahertz52 <badahertz52@gmail.com>
2024-09-06 13:51:28 +0900

    feat: 디바운스 유틸 생성 및 스크롤에 디바운스 적용

--- src/pages/Home/hooks/useScrollEvent.tsx
+++ src/pages/Home/hooks/useScrollEvent.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
+import { debounce } from '../../../utils/debounce';
 
 type ScrollHandler = () => void;
+const DEBOUNCE_DELAY = 50;
 
 const useScrollEvent = (onScroll: ScrollHandler) => {
   useEffect(() => {
+    const debouncedScrollHandler = debounce(onScroll, DEBOUNCE_DELAY);
+
     const handleScroll = (event: Event) => {
-      onScroll();
+      debouncedScrollHandler();
     };
 
     window.addEventListener('scroll', handleScroll);
