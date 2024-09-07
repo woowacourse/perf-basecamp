0a0d1ca2f7e1968c6ba2ef88f2c7ea86748d0c07
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-09-07 18:35:34 +0900

    feat: browserRouter로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
 import NavBar from './components/NavBar/NavBar';
