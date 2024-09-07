258e63e989121764f359b0c76d758111a60fff74
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-09-07 18:09:09 +0900

    feat: 이미지 최적화 적용

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -2,15 +2,18 @@ import styles from './FeatureItem.module.css';
 
 type FeatureItemProps = {
   title: string;
-  imageSrc: string;
+  mp4Src?: string;
+  webmSrc: string;
 };
 
-const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
+const FeatureItem = ({ title, webmSrc, mp4Src }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
-      <div className={styles.featureTitleBg}></div>
-      <h4 className={styles.featureTitle}>{title}</h4>
+      <video className={styles.featureImage} autoPlay loop muted playsInline>
+        {webmSrc && <source src={webmSrc} type="video/mp4" />}
+        {!webmSrc && mp4Src && <source src={mp4Src} type="video/mp4" />}
+        <h4 className={styles.featureTitle}>{title}</h4>
+      </video>
     </div>
   );
 };
