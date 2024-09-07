d84ce557d26c707b245628b0cda55097496693a0
Kim Da Eun <midekuna@gmail.com>
2024-09-07 16:47:44 +0900

    feat: 검색 결과 memoization

--- src/pages/Search/components/SearchResult/SearchResult.tsx
+++ src/pages/Search/components/SearchResult/SearchResult.tsx
@@ -1,7 +1,8 @@
+import React, { useMemo } from 'react';
 import { GifImageModel } from '../../../../models/image/gifImage';
 
-import ResultTitle from '../ResultTitle/ResultTitle';
 import GifItem from '../GifItem/GifItem';
+import ResultTitle from '../ResultTitle/ResultTitle';
 
 import { SearchStatus, SEARCH_STATUS } from '../../hooks/useGifSearch';
 
@@ -13,13 +14,18 @@ type SearchResultProps = {
   loadMore: () => void;
 };
 
+const MemoizedGifItem = React.memo(GifItem);
+
 const SearchResult = ({ status, gifList, loadMore }: SearchResultProps) => {
-  const renderGifList = () => (
-    <div className={styles.gifResultWrapper}>
-      {gifList.map((gif: GifImageModel) => (
-        <GifItem key={gif.id} imageUrl={gif.imageUrl} title={gif.title} />
-      ))}
-    </div>
+  const renderGifList = useMemo(
+    () => (
+      <div className={styles.gifResultWrapper}>
+        {gifList.map((gif: GifImageModel) => (
+          <MemoizedGifItem key={gif.id} imageUrl={gif.imageUrl} title={gif.title} />
+        ))}
+      </div>
+    ),
+    [gifList]
   );
 
   const renderLoadMoreButton = () => (
@@ -33,12 +39,12 @@ const SearchResult = ({ status, gifList, loadMore }: SearchResultProps) => {
       case SEARCH_STATUS.FOUND:
         return (
           <>
-            {renderGifList()}
+            {renderGifList}
             {renderLoadMoreButton()}
           </>
         );
       case SEARCH_STATUS.BEFORE_SEARCH:
-        return renderGifList();
+        return renderGifList;
       case SEARCH_STATUS.NO_RESULT:
       case SEARCH_STATUS.ERROR:
       default:
