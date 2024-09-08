970ac81e3ed802b1e29dd5cee77d57b86b2841b7
Seongjin Hong <hongseongjin.to@gmail.com>
2024-09-08 17:59:03 +0900

    feat: trending API 요청/응답 데이터의 캐싱 처리 구현

---
+++ src/utils/cacheService.ts
@@ -0,0 +1,60 @@
+const DEFAULT_CACHE_DURATION = 1000 * 60 * 60;
+
+export default class CacheService {
+  private readonly cacheName: string;
+  private readonly defaultCacheDuration: number;
+
+  constructor({
+    cacheName,
+    defaultCacheDuration = DEFAULT_CACHE_DURATION
+  }: {
+    cacheName: string;
+    defaultCacheDuration?: number;
+  }) {
+    this.cacheName = cacheName;
+    this.defaultCacheDuration = defaultCacheDuration;
+  }
+
+  async set(
+    key: string,
+    value: any,
+    cacheDuration: number = this.defaultCacheDuration
+  ): Promise<void> {
+    const cache = await caches.open(this.cacheName);
+    const response = new Response(
+      JSON.stringify({
+        value,
+        cacheExpireTime: Date.now() + cacheDuration
+      })
+    );
+
+    await cache.put(key, response);
+  }
+
+  async get<T>(key: string): Promise<T | null> {
+    const cache = await caches.open(this.cacheName);
+    const response = await cache.match(key);
+
+    if (!response) {
+      return null;
+    }
+
+    const data = await response.json();
+
+    if (Date.now() > data.expiry) {
+      await cache.delete(key);
+      return null;
+    }
+
+    return data.value;
+  }
+
+  async delete(key: string): Promise<void> {
+    const cache = await caches.open(this.cacheName);
+    await cache.delete(key);
+  }
+
+  async clear(): Promise<void> {
+    await caches.delete(this.cacheName);
+  }
+}
