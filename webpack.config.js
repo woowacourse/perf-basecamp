bb8e83608fd985593e70026cfc62890cac42d3b2
useon <roqkftjs@gmail.com>
2024-09-08 14:34:21 +0900

    refactor: 파일명에 contenthash 적용으로 캐싱 최적화

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.[chunkhash].js',
+    filename: '[name].bundle.[contenthash].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
