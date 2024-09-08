e3e856c42196c0d621d4e4e4b100c21e9b8a9e38
jayming66 <mjw8941@gmail.com>
2024-09-08 23:51:10 +0900

    refactor(FeatureItem): heading 순서에 맞게 변경

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -10,7 +10,7 @@ const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
     <div className={styles.featureItem}>
       <video className={styles.featureImage} src={imageSrc} autoPlay loop muted />
       <div className={styles.featureTitleBg}></div>
-      <h4 className={styles.featureTitle}>{title}</h4>
+      <h3 className={styles.featureTitle}>{title}</h3>
     </div>
   );
 };
