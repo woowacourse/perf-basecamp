f67d4819e3d6da0d864afe4428473e833a4ffcb3
soosoo22 <lllllh00@naver.com>
2024-09-08 13:01:56 +0900

    chore: 번들 파일 이름에 contenthash 추가

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].bundle.js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
