1e1181e485ac954a50d57a80473f61c7e4a81758
pakxe <pigkill40@naver.com>
2024-09-09 15:58:54 +0900

    feat: 이미지 불러오는 api를 사용하도록 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,6 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
-
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
 import AnimatedPath from './components/AnimatedPath/AnimatedPath';
@@ -18,10 +13,15 @@ const cx = classNames.bind(styles);
 const Home = () => {
   const wrapperRef = useRef<HTMLElement>(null);
 
+  const heroImageUrl = `${process.env.IMAGE_URL}/hero.avif`;
+  const trendingGifUrl = `${process.env.IMAGE_URL}/trending.avif`;
+  const findGifUrl = `${process.env.IMAGE_URL}/find.avif`;
+  const freeGifUrl = `${process.env.IMAGE_URL}/free.avif`;
+
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <img className={styles.heroImage} src={heroImageUrl} alt="hero image" />
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -35,9 +35,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" imageSrc={trendingGifUrl} />
+            <FeatureItem title="Find gif for free" imageSrc={findGifUrl} />
+            <FeatureItem title="Free for everyone" imageSrc={freeGifUrl} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
