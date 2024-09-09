135d4f4f7745164ebc8348f7fef5d15e86e9f2d4
hwinkr <dnddl8280@naver.com>
2024-09-09 15:32:37 +0900

    chore: 커스텀 커서의 텍스트를 "쑤쑤나는해리야"로 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,10 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImage from '../../assets/images/hero.png?as=webp';
+import trendingGif from '../../assets/images/trending.gif?as=webp';
+import findGif from '../../assets/images/find.gif?as=webp';
+import freeGif from '../../assets/images/free.gif?as=webp';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -23,7 +23,7 @@ const Home = () => {
       <section className={styles.heroSection}>
         <img className={styles.heroImage} src={heroImage} alt="hero image" />
         <div className={styles.projectTitle}>
-          <h1 className={styles.title}>Memegle</h1>
+          <h1 className={styles.title}>memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
         </div>
         <Link to="/search">
@@ -44,7 +44,7 @@ const Home = () => {
           </Link>
         </div>
       </section>
-      <CustomCursor text="memegle" />
+      <CustomCursor text="쑤쑤나는해리야" />
     </>
   );
 };
