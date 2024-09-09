e6fd03e25873cd4feceabd8f72bf116e56910c09
pakxe <pigkill40@naver.com>
2024-09-09 15:56:36 +0900

    feat: 번들 코드 최적화 적용

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -24,7 +28,14 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      generateStatsFile: true,
+      statsFilename: 'bundle-report.json'
+    }),
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
@@ -37,7 +48,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -49,6 +60,26 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      '...',
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            // 가능한 모든 코드를 압축
+            drop_console: true, // 콘솔 로그를 제거하여 파일 크기 감소
+            drop_debugger: true, // 디버거 코드 제거
+            pure_funcs: ['console.info'] // 특정 함수 호출 제거
+          },
+          mangle: true, // 변수 및 함수 이름을 짧게 변경
+          output: {
+            comments: false // 주석을 제거
+          }
+        },
+        extractComments: false, // 별도의 파일로 주석을 추출하지 않음
+        parallel: true // 멀티 프로세스를 사용하여 빌드 속도 향상
+      })
+    ]
   }
 };
