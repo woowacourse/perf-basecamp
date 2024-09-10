eefd553db4ea342a65d9a48c8638916c1ec68931
brgndy <brgndy96@gmail.com>
2024-09-10 17:06:34 +0900

    fix: devtool 소스맵 false 추가

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: false,
   plugins: [
     new BundleAnalyzerPlugin({
       defaultSizes: 'gzip'
