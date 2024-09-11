95b04bb8c9e334940e5713a21d4d1917b4a40845
rbgksqkr <rbgks1937@naver.com>
2024-09-12 01:27:16 +0900

    refactor: react-windows 로 렌더링 최적화

--- src/pages/Search/components/ArtistList/ArtistList.tsx
+++ src/pages/Search/components/ArtistList/ArtistList.tsx
@@ -1,5 +1,6 @@
 import { Artist } from '../../../../models/help/artist';
 import ArtistInfo from '../ArtistInfo/ArtistInfo';
+import { FixedSizeList as List } from 'react-window';
 
 type ArtistListProps = {
   artists: Artist[];
@@ -7,11 +8,18 @@ type ArtistListProps = {
 
 const ArtistList = ({ artists }: ArtistListProps) => {
   return (
-    <ul>
-      {artists.map((artist, index) => {
-        return <ArtistInfo key={index} artist={artist} />;
-      })}
-    </ul>
+    <List
+      height={400} // 리스트의 총 높이 (보이는 영역)
+      itemCount={artists.length} // 전체 아이템 개수
+      itemSize={50} // 각 아이템의 고정 높이
+      width={'100%'} // 리스트의 너비
+    >
+      {({ index, style }) => (
+        <div style={style}>
+          <ArtistInfo artist={artists[index]} />
+        </div>
+      )}
+    </List>
   );
 };
 
