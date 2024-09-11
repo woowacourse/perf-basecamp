5ae4d6fa94afbd58b1f9fa599801b96b43b8959a
badahertz52 <badahertz52@gmail.com>
2024-09-11 16:38:45 +0900

    docs: 불필요한 image module 삭제

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -5,18 +5,3 @@ declare module '*.svg';
 declare module '*.webp';
 declare module '*as=webp';
 declare module '*as=jpg';
-
-interface ResponsiveImageOutput {
-  src: string;
-  srcSet: string;
-  placeholder: string | undefined;
-  images: { path: string; width: number; height: number }[];
-  width: number;
-  height: number;
-  toString: () => string;
-}
-
-declare module '*.responsive.*' {
-  const src: ResponsiveImageOutput;
-  export default src;
-}
