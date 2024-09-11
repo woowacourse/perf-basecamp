dfe5382d3d47574ca19cfeb9b78f4913010496d2
cys4585 <dev.youngsu@gmail.com>
2024-09-11 20:48:44 +0900

    추가: 3. 최소한의 변경만 일으키기 > lazy loading으로 인한 layout shift 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div style={{ height: '100vh' }}>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
