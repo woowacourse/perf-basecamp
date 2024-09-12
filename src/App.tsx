5f665893438ba9b5c5e3c5654889d0f256ec7a3e
greetings1012 <ahnqwerty@gmail.com>
2024-09-12 22:07:41 +0900

    refactor: Home 페이지에도 Suspense와 lazy 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
-import Home from './pages/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 import Fallback from './components/Fallback/Fallback';
 
+const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
