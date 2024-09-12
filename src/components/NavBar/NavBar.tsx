77ef0c2e44f6d22dcbeaedb00641fb9e9bfe7a56
hwinkr <dnddl8280@naver.com>
2024-09-12 22:35:43 +0900

    chore: 링크 이름 수정

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <header className={styles.header}>
       <nav className={styles.nav}>
         <Link to="/">
-          <span className={styles.logo}>memegle</span>
+          <span className={styles.logo}>Memegle</span>
         </Link>
         <Link to="/search">
           <button className={styles.searchPageButton}>start search</button>
