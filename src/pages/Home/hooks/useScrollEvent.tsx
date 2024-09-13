748e89f9a72a48a3740a36211723ac37c12c1fb8
soosoo22 <lllllh00@naver.com>
2024-09-13 11:38:16 +0900

    refactor: requestAnimationFrame과 cancelAnimationFrame을 사용하여 스크롤 이벤트 핸들링 최적화

--- src/pages/Home/hooks/useScrollEvent.tsx
+++ src/pages/Home/hooks/useScrollEvent.tsx
@@ -1,16 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type ScrollHandler = () => void;
 
 const useScrollEvent = (onScroll: ScrollHandler) => {
+  const requestRef = useRef<number | null>(null);
+
   useEffect(() => {
-    const handleScroll = (event: Event) => {
-      onScroll();
+    const handleScroll = () => {
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
+
+      requestRef.current = requestAnimationFrame(() => {
+        onScroll();
+
+        requestRef.current = null;
+      });
     };
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
+      if (requestRef.current) {
+        cancelAnimationFrame(requestRef.current);
+      }
+
       window.removeEventListener('scroll', handleScroll);
     };
   }, [onScroll]);
