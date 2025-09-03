78c96d21397ee5fb307471a8d6def97d77006bb7
Beomtae <region9@naver.com>
2025-09-03 14:25:37 +0900

    ♻️ refactor: 성능 최적화를 위해 CSS 소스코드 압축

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -50,5 +51,6 @@ module.exports = {
   },
   optimization: {
     minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()]
   }
 };
