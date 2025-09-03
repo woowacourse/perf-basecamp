766fabae908f51480a79f80acd7dad219bf0a058
eunoia-jaxson <eunoia@handong.ac.kr>
2025-09-03 13:41:21 +0900

    chore: webpack 설정 최적화 및 코드 분할을 위한 lazy loading 추가

--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,68 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/dist'),
-    clean: true
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true
-  },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './public', to: './public' }]
-    }),
-    new Dotenv()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
+module.exports = (_, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/index.tsx',
+    resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+    output: {
+      filename: isProd ? 'static/js/[name].[contenthash:8].js' : 'static/js/[name].js',
+      chunkFilename: isProd
+        ? 'static/js/[name].[contenthash:8].chunk.js'
+        : 'static/js/[name].chunk.js',
+      path: path.join(__dirname, '/dist'),
+      clean: true
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true
+    },
+    devtool: isProd ? 'source-map' : 'eval-cheap-module-source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './public', to: './public' }]
+      }),
+      new Dotenv()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader'
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff2?)$/i,
+          type: 'asset',
+          parser: {
+            dataUrlCondition: {
+              maxSize: 8 * 1024
+            }
+          },
+          generator: {
+            filename: 'static/[name].[contenthash][ext]'
+          }
         }
+      ]
+    },
+    optimization: {
+      minimize: isProd,
+      splitChunks: {
+        chunks: 'all'
       },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: false
-  }
+      runtimeChunk: 'single'
+    }
+  };
 };
