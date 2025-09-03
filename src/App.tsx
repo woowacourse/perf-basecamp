ad889a90a8d0cb2f51f9f964462bc0fbd782b5d3
Beomtae <region9@naver.com>
2025-09-03 16:26:54 +0900

    🔨 chore: serve 스크립트 명령어 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { value BrowserRouter as Router, value Route, value Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Search from './pages/Search/Search';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -48,9 +47,5 @@ module.exports = {
         }
       }
     ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [`...`, new CssMinimizerPlugin()]
   }
 };
