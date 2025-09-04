a4df8ce5934010d658e3e98c46fca81f877f894a
eunoia-jaxson <eunoia@handong.ac.kr>
2025-09-04 15:11:29 +0900

    fix: Home 컴포넌트를 직접 import하고 CSS에서 최소 높이 속성을 제거하여 레이아웃 개선

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 
 import './App.css';
 
-const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
