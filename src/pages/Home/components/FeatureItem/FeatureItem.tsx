f40ebb6735be39ab25e11240c60d4ba7be643850
eunwoo-levi <eunwoo1341@gmail.com>
2025-09-05 14:51:24 +0900

    feat: featureItem Image 최적화

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -8,7 +8,16 @@ type FeatureItemProps = {
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      <div className={styles.featureImageBox}>
+        <img
+          className={styles.featureImage}
+          src={imageSrc}
+          alt={`${title} feature illustration`}
+          loading="lazy"
+          decoding="async"
+        />
+      </div>
+
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
