29f640bc81fee348aa6d925a4955a06502539720
haku <hakukudh@gmail.com>
2025-09-05 15:16:06 +0900

    refactor: 이미지 최적화

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImageWebp from '../../assets/images/hero.webp';
+import trendingGifWebp from '../../assets/images/trending.webm';
+import trendingGifMp4 from '../../assets/images/trending.mp4';
+import findGifWebp from '../../assets/images/find.webm';
+import findGifMp4 from '../../assets/images/find.mp4';
+import freeGifWebp from '../../assets/images/free.webm';
+import freeGifMp4 from '../../assets/images/free.mp4';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -21,7 +25,10 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source srcSet={heroImageWebp} type="image/webp" />
+          <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -35,9 +42,13 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem
+              title="See trending gif"
+              mp4Src={trendingGifMp4}
+              webmSrc={trendingGifWebp}
+            />
+            <FeatureItem title="Find gif for free" mp4Src={findGifMp4} webmSrc={findGifWebp} />
+            <FeatureItem title="Free for everyone" mp4Src={freeGifMp4} webmSrc={freeGifWebp} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
