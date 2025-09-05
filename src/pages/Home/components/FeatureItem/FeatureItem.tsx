71a3eda298ca8073d026197cb88ba4252371fe4f
deokbin <deokbin@naver.com>
2025-09-05 09:14:46 +0900

    feat: webp와 mp4 모두 처리할 수 있도록 태그 추가

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -6,9 +6,15 @@ type FeatureItemProps = {
 };
 
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
+  const isVideo = imageSrc.endsWith('.mp4');
+
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      {isVideo ? (
+        <video className={styles.featureImage} src={imageSrc} autoPlay loop muted playsInline />
+      ) : (
+        <img className={styles.featureImage} src={imageSrc} alt={title} loading="lazy" />
+      )}
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
