11af8d837d5973a2cfcf3a4c68a505f954a5f18b
thgml05 <aisohii@naver.com>
2025-09-05 11:01:35 +0900

    chore: gif -> mp4 변환 및 압축

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|jpg|gif|mp4)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
