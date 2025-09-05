1bb39140c3e703d37c464253e85edaaa14e0f344
Minje Sung <minje813@dgu.ac.kr>
2025-09-05 20:53:38 +0900

    refactor: hero 이미지 교체

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,8 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
+import heroImageWebp from '../../assets/images/hero.webp';
+import heroImageJpg from '../../assets/images/hero.jpg';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
 
@@ -21,7 +21,10 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <picture>
+          <source srcSet={heroImageWebp} type="image/webp" />
+          <img className={styles.heroImage} src={heroImageJpg} alt="hero image" />
+        </picture>
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
