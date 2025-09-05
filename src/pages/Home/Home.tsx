03d65719acfaf0ba54ec6b18745311b064d836a7
Minje Sung <minje813@dgu.ac.kr>
2025-09-05 20:53:50 +0900

    refactor: gif 교체

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,9 @@ import classNames from 'classnames/bind';
 
 import heroImageWebp from '../../assets/images/hero.webp';
 import heroImageJpg from '../../assets/images/hero.jpg';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import trendingMp4 from '../../assets/images/trending.mp4';
+import findMp4 from '../../assets/images/find.mp4';
+import freeMp4 from '../../assets/images/free.mp4';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -38,9 +39,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" videoSrc={trendingMp4} />
+            <FeatureItem title="Find gif for free" videoSrc={findMp4} />
+            <FeatureItem title="Free for everyone" videoSrc={freeMp4} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
