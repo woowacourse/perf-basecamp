e9fde52458b232eccabaeb0bf20906d9e81e4bf5
dev-dino22 <wlsskdud02@naver.com>
2025-09-06 22:57:34 +0900

    refactor: gif 요청을 webp 로 먼저 시도하도록 api 수정

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -24,7 +24,7 @@ const convertResponseToModel = (gifList: IGif[]): GifImageModel[] => {
     return {
       id,
       title: title ?? '',
-      imageUrl: images.original.url
+      imageUrl: images.original.webp ?? images.original.url
     };
   });
 };
