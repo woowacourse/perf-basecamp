168dc805d8945a0dc662e1dd6754e51d55bac0dd
Beomtae <region9@naver.com>
2025-09-07 13:58:11 +0900

    ♻️ refactor: 이미지 파일 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,10 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImage from '../../assets/images/hero.webp';
+import trendingGif from '../../assets/images/trending.webp';
+import findGif from '../../assets/images/find.webp';
+import freeGif from '../../assets/images/free.webp';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -21,11 +21,7 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <picture>
-          <source srcSet="/static/hero.avif" type="image/avif" />
-          <source srcSet="/static/hero.webp" type="image/webp" />
-          <img className={styles.heroImage} src={heroImage} alt="hero image" />
-        </picture>
+        <img className={styles.heroImage} src={heroImage} alt="hero image" />
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
