599e5fee42685e055c860c61aeab7772ff4a4989
Oliy <eunsoa0310@gmail.com>
2025-09-08 10:36:41 +0900

    refactor: Enhance FeatureItem component structure

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -8,7 +8,9 @@ type FeatureItemProps = {
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      <div className={styles.featureImageContainer}>
+        <img className={styles.featureImage} src={imageSrc} alt={title} />
+      </div>
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
