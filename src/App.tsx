480ff64ee1e332b61c20b5042dae4c85c4c9941a
eunoia-jaxson <eunoia@handong.ac.kr>
2025-09-08 13:50:07 +0900

    feat: Home 컴포넌트를 lazy loading으로 변경하고, Suspense의 fallback을 개선하여 로딩 시 사용자 경험 향상

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home';
 
 import './App.css';
 
+const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Suspense fallback={null}>
+      <Suspense fallback={<div style={{ height: '100vh', width: '100vw' }} />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
