c29b08f544709ca5ad25219a03940f78ce21f8e8
kimyou1102 <yoosk5485@naver.com>
2025-09-08 14:35:31 +0900

    refactor: 캐시스토리지로 trending API 캐싱 구현하도록 변경
    - 10분을 캐싱하게 해서 새로고침해도 캐싱이 유지되도록 변경

--- src/pages/Search/hooks/useGifSearch.tsx
+++ src/pages/Search/hooks/useGifSearch.tsx
@@ -15,8 +15,6 @@ export const SEARCH_STATUS = {
 
 export type SearchStatus = typeof SEARCH_STATUS[keyof typeof SEARCH_STATUS];
 
-let giftListCache: GifImageModel[] | null = null;
-
 const useGifSearch = () => {
   const [status, setStatus] = useState<SearchStatus>(SEARCH_STATUS.BEFORE_SEARCH);
   const [currentPageIndex, setCurrentPageIndex] = useState(DEFAULT_PAGE_INDEX);
@@ -74,11 +72,9 @@ const useGifSearch = () => {
   useEffect(() => {
     const fetchTrending = async () => {
       if (status !== SEARCH_STATUS.BEFORE_SEARCH) return;
-      if (giftListCache !== null) return;
 
       try {
         const gifs = await gifAPIService.getTrending();
-        giftListCache = gifs;
         setGifList(gifs);
       } catch (error) {
         handleError(error);
