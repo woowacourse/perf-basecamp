2711685ecd1281f9331dd7c82062740450fc0fbb
Beomtae <region9@naver.com>
2025-09-08 22:24:50 +0900

    ✨ feat: 반응형 이미지 최적화 및 이미지 해상도 최적화

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,12 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
+import heroImage2000 from '../../assets/images/hero-2000.png';
+import heroImage1600 from '../../assets/images/hero-1600.png';
+import heroImage1200 from '../../assets/images/hero-1200.png';
+import heroImage768 from '../../assets/images/hero-768.png';
+import heroImage768Mobile from '../../assets/images/hero-mobile-768.png';
+import heroImage480Mobile from '../../assets/images/hero-mobile-480.png';
 import trendingGif from '../../assets/images/trending.webp';
 import findGif from '../../assets/images/find.webp';
 import freeGif from '../../assets/images/free.webp';
@@ -22,11 +27,50 @@ const Home = () => {
     <>
       <section className={styles.heroSection}>
         <picture>
-          <source srcSet="/static/hero.avif" type="image/avif" />
-          <source srcSet="/static/hero.webp" type="image/webp" />
+          <source
+            type="image/avif"
+            media="(max-width: 768px)"
+            srcSet="
+      /static/hero-mobile-480.avif 480w,
+      /static/hero-mobile-768.avif 768w"
+            sizes="100vw"
+          />
+          <source
+            type="image/webp"
+            media="(max-width: 768px)"
+            srcSet="
+      /static/hero-mobile-480.webp 480w,
+      /static/hero-mobile-768.webp 768w"
+            sizes="100vw"
+          />
+          <source
+            type="image/avif"
+            srcSet="
+      /static/hero-768.avif 768w,
+      /static/hero-1200.avif 1200w,
+      /static/hero-1600.avif 1600w,
+      /static/hero-2000.avif 2000w"
+            sizes="(max-width: 1200px) 90vw, 1200px"
+          />
+          <source
+            type="image/webp"
+            srcSet="
+      /static/hero-768.webp 768w,
+      /static/hero-1200.webp 1200w,
+      /static/hero-1600.webp 1600w,
+      /static/hero-2000.webp 2000w"
+            sizes="(max-width: 1200px) 90vw, 1200px"
+          />
           <img
             className={styles.heroImage}
-            src={heroImage}
+            src={
+              heroImage2000 ||
+              heroImage1600 ||
+              heroImage1200 ||
+              heroImage768 ||
+              heroImage768Mobile ||
+              heroImage480Mobile
+            }
             fetchPriority="high"
             decoding="async"
             loading="eager"
