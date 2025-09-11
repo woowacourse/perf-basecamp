075a68ffa04e08512f3ebfd41157d72adcf1fa39
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-09-11 21:25:14 +0900

    refactor: Webpack 설정을 함수형으로 변경하여 환경에 따른 로더 최적화

--- webpack.config.js
+++ webpack.config.js
@@ -7,95 +7,98 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/dist'),
-    clean: true
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true
-  },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './public', to: './public' }]
-    }),
-    new Dotenv(),
-    new MiniCssExtractPlugin(),
-    new BundleAnalyzerPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      '...',
-      new CssMinimizerPlugin(),
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.sharpMinify,
-          options: {
-            encodeOptions: {
-              png: { quality: 80 },
-              jpeg: { quality: 80 }
-            }
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    entry: './src/index.tsx',
+    resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, '/dist'),
+      clean: true
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true
+    },
+    devtool: 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './public', to: './public' }]
+      }),
+      new Dotenv(),
+      new MiniCssExtractPlugin(),
+      new BundleAnalyzerPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader'
           }
         },
-        generator: [
-          {
-            preset: 'webp-pc',
-            implementation: ImageMinimizerPlugin.sharpGenerate,
+        {
+          test: /\.css$/i,
+          use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader']
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'static/[name].[ext]'
+          }
+        }
+      ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        '...',
+        new CssMinimizerPlugin(),
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.sharpMinify,
             options: {
               encodeOptions: {
-                webp: {
-                  quality: 50
-                }
-              },
-              resize: { enabled: true, width: 1920 }
+                png: { quality: 80 },
+                jpeg: { quality: 80 }
+              }
             }
           },
-          {
-            preset: 'avif-pc',
-            implementation: ImageMinimizerPlugin.sharpGenerate,
-            options: {
-              encodeOptions: {
-                avif: {
-                  quality: 50
-                }
-              },
-              resize: { enabled: true, width: 1920 }
+          generator: [
+            {
+              preset: 'webp-pc',
+              implementation: ImageMinimizerPlugin.sharpGenerate,
+              options: {
+                encodeOptions: {
+                  webp: {
+                    quality: 50
+                  }
+                },
+                resize: { enabled: true, width: 1920 }
+              }
+            },
+            {
+              preset: 'avif-pc',
+              implementation: ImageMinimizerPlugin.sharpGenerate,
+              options: {
+                encodeOptions: {
+                  avif: {
+                    quality: 50
+                  }
+                },
+                resize: { enabled: true, width: 1920 }
+              }
             }
-          }
-        ]
-      })
-    ]
-  }
+          ]
+        })
+      ]
+    }
+  };
 };
