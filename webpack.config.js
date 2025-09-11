77f73e3846e79bf06571e29375f429caf2300c4c
haku <168343874+ha-kuku@users.noreply.github.com>
2025-09-11 09:45:39 +0900

    [개인 미션 - 성능 오답노트] 하쿠(이동현) 미션 제출합니다. (#180)
    * build: 성능 개선 전 배포
    
    * chore: 배포 전 환경설정
    
    * chore: 번들크기 압축 설정
    
    * refactor: 이미지 최적화
    
    * refactor: 코드 스플릿팅 적용
    
    * refactor: trending 데이터 캐싱 처리
    
    * refactor: gifItem 메모이제이션을 통한 리렌더링 방지
    
    * refactor: layout shift 방지

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.tsx',
-  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      'react-dom$': 'react-dom/profiling'
+    }
+  },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.join(__dirname, '/dist'),
     clean: true
   },
@@ -24,7 +30,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin()
   ],
   module: {
     rules: [
@@ -40,7 +47,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp|mp4|webm)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
@@ -49,6 +56,6 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true
   }
 };
