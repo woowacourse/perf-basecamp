ffefaf6743417c313b65d08987eeaacbc075a30d
Luna <97431021+ShinjungOh@users.noreply.github.com>
2025-09-12 16:11:50 +0900

    [개인 미션 - 성능 오답노트] 루나(오신정) 미션 제출합니다. (#177)
    * chore: 배포 주소 설정 및 gitignore에 idea 추가
    
    * chore: webpack-bundle-analyzer 설치 및 세팅
    
    * refactor: @react-icons/all-files 설치 및 실사용 아이콘만 import
    
    * refactor: Code Splitting 적용해 Home과 Search 페이지 분리
    
    * refactor: terser-webpack-plugin 설치 및 소스코드 최적화
    
    * feat: hero 이미지 webp로 변환 및 최적화
    
    * refactor: Trending API 메모리 캐싱 적용
    
    * feat: GifItem에 메모이제이션 적용
    
    * style: prettier 적용
    
    * refactor: Layout Shift 제거
    
    * refactor: transition: all -> transform 변경
    
    * feat: img에 alt 속성 추가
    
    * feat: GifItem, HelpPanel img에 alt 속성 추가
    
    * chore: CSS 최적화 플러그인 설정
    
    * feat: images.d.ts에 webm 추가
    
    * feat: sharp 적용해 이미지 최적화
    
    * style: prettier 적용
    
    * chore: sharp 삭제 및 설정 롤백
    
    * chore: 사용하지 않는 확장자 타입 제거
    
    * fix: filename에 contenthash 추가
    
    * refactor: 폰트 비동기 로드
    
    * refactor: LCP 이미지에 fetchPriority 속성 추가
    
    * refactor: h1, footer CLS 개선
    
    * refactor: 폰트 &subset=latin 적용
    
    * refactor: footer에 position 추가해 CLS 개선
    
    * refactor: h1, footer CLS 개선
    
    * fix: Suspense 태그 위치 수정

--- src/pages/Home/components/FeatureItem/FeatureItem.tsx
+++ src/pages/Home/components/FeatureItem/FeatureItem.tsx
@@ -8,7 +8,7 @@ type FeatureItemProps = {
 const FeatureItem = ({ title, imageSrc }: FeatureItemProps) => {
   return (
     <div className={styles.featureItem}>
-      <img className={styles.featureImage} src={imageSrc} />
+      <img className={styles.featureImage} src={imageSrc} alt={title} />
       <div className={styles.featureTitleBg}></div>
       <h4 className={styles.featureTitle}>{title}</h4>
     </div>
