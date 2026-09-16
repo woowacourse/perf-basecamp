ef4a5e638df8fe1f900a31245c2c5152c047386a
김상윤 <ksy151020@gmail.com>
2026-09-16 19:41:58 +0900

    fix(소스코드 최적화): uglify작업을 위한 webpack 설정의 mode를 'production'으로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.tsx',
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
