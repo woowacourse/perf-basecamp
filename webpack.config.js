be9eeedb31118875b2d4e2110a0787370fcbbc88
binggwa <qudrhks317@gmail.com>
2026-09-16 13:20:50 +0900

    chore: webp 타입 인식을 위한 types 설정 및 webpack 압축 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|webp)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
