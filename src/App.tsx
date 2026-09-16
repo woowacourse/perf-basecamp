a0306dfb8090c6e9666d31a775e9c17a4b4df137
jebiyeon02 <jhje5595@naver.com>
2026-09-16 23:29:22 +0900

    chore: 해시 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Search from './pages/Search/Search';
