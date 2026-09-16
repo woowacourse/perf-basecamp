f44167c8db7e36e7bddff7e6bb9f3d61d5bbbc80
binggwa <qudrhks317@gmail.com>
2026-09-16 12:27:06 +0900

    perf(webpack): 번들 크기 및 이미지 용량 최적화 적용

--- webpack.config.js
+++ webpack.config.js
@@ -40,15 +40,44 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2)$/i,
         loader: 'file-loader',
         options: {
           name: 'static/[name].[ext]'
         }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'static/[name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              // PNG 압축 옵션
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              // GIF 최적화 옵션
+              gifsicle: {
+                interlaced: false
+              },
+              // WebP 포맷으로 강제 변환 및 압축 옵션
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
       }
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true
   }
 };
