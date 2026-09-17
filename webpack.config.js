3c66af0b37489ae26324d28d789d7c56f36ab589
kojesung <js95112345@gmail.com>
2026-09-17 11:10:44 +0900

    chore: 번들 파일에서 css 추출 및 최적화한 후 HtmlWebpackPlugin이 link 태그로 삽입하도록 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,61 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/dist'),
-    clean: true
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    historyApiFallback: true
-  },
-  devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './public', to: './public' }]
-    }),
-    new Dotenv()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: './src/index.tsx',
+    resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, '/dist'),
+      clean: true
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      historyApiFallback: true
+    },
+    devtool: 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './public', to: './public' }]
+      }),
+      new Dotenv(),
+      ...(isProd ? [new MiniCssExtractPlugin()] : [])
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader'
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader']
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+          loader: 'file-loader',
+          options: {
+            name: 'static/[name].[ext]'
+          }
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'static/[name].[ext]'
-        }
-      }
-    ]
-  }
+      ]
+    },
+    optimization: {
+      minimizer: ['...', new CssMinimizerPlugin()]
+    }
+  };
 };
