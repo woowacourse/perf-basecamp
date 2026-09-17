68ef4e4e748082851e04fd6324fc040cfc572798
stardusty-lab <startdusty.lab@gmail.com>
2026-09-17 13:01:03 +0900

    Revert "feat(App): Router의 basename /로 변경"
    This reverts commit 73df8393c3f653ca05d81b5b460f71140556d386.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router basename={'/'}>
+    <Router basename={'/perf-basecamp'}>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
