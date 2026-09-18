01fa03eb35a2f548caeea257cb3c207e0602b5f2
kojesung <js95112345@gmail.com>
2026-09-18 19:55:52 +0900

    chore: Search 페이지에 지연 로딩 적용(청크 이름 search로 지정)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+
+// Home 진입 시 Search 코드를 받지 않도록 라우트 단위로 분리
+const Search = lazy(
+  async () => await import(/* webpackChunkName: "search" */ './pages/Search/Search')
+);
 
 const App = () => {
   return (
@@ -14,7 +19,14 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search"
+          element={
+            <Suspense fallback={<p>검색 페이지 불러오는 중..</p>}>
+              <Search />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
