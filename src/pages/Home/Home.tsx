8800e70cbfc09276d1e5323d9eaa445012c590c3
stardusty-lab <startdusty.lab@gmail.com>
2026-09-18 21:03:48 +0900

    chore(package.json): performance scripts 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,9 +5,9 @@ import classNames from 'classnames/bind';
 import heroImage_1920_65Avif from '../../assets/images/hero-1920-65.avif';
 import heroImage_1920_40M6Webp from '../../assets/images/hero-1920-40-m6.webp';
 import heroImage_1920_20Jpg from '../../assets/images/hero-1920-20.jpg';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import trendingWebp from '../../assets/images/trending-30.webp';
+import findWebp from '../../assets/images/find-30.webp';
+import freeWebp from '../../assets/images/free-30.webp';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -41,9 +41,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" imageSrc={trendingWebp} />
+            <FeatureItem title="Find gif for free" imageSrc={findWebp} />
+            <FeatureItem title="Free for everyone" imageSrc={freeWebp} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
