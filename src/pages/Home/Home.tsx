18d6b6b75a497f92312f3f35d717e8bb3d1a72af
jebiyeon02 <jhje5595@naver.com>
2026-09-19 17:08:57 +0900

    refactor: 히어로 이미지를 avif 확장자로 변경하고 사용자 화면 크기에 적합한 이미지 제공

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,11 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
+import hero640 from '../../assets/images/hero/hero-640.avif';
+import hero960 from '../../assets/images/hero/hero-960.avif';
+import hero1280 from '../../assets/images/hero/hero-1280.avif';
+import hero1920 from '../../assets/images/hero/hero-1920.avif';
+import hero2560 from '../../assets/images/hero/hero-2560.avif';
 import trendingGif from '../../assets/images/trending.gif';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
@@ -21,7 +25,19 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <img
+          className={styles.heroImage}
+          src={hero1280}
+          srcSet={`
+            ${hero640} 640w,
+            ${hero960} 960w,
+            ${hero1280} 1280w,
+            ${hero1920} 1920w,
+            ${hero2560} 2560w
+            `}
+          sizes="100vw"
+          alt="hero image"
+        />
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
