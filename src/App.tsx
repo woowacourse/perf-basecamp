4dca21bf7888cf3075b8ab28814a73e713cee910
박건규 <geongyu09@gmail.com>
2026-09-19 19:54:54 +0900

    fix: 번들 크기 줄이기
    - minimize, usedExports 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
+// import Home from './pages/Home/Home';
+// import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Search = lazy(() => import('./pages/Search/Search'));
 
 import './App.css';
 
 const App = () => {
   return (
-    // <Router basename={'/perf-basecamp'}>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router basename={'/perf-basecamp'}>
+        {/* <Router> */}
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
