f41ecc3f375787c14f44887f497daaeb2c4d0c22
yundoll <mar0722@naver.com>
2026-09-19 20:04:28 +0900

    perf: code-spliting 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
+import { lazy, Suspense } from 'react';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+const Home = lazy(async () => await import('./pages/Home/Home'));
+const Search = lazy(async () => await import('./pages/Search/Search'));
+
 const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
-    </Router>
+    // <Router>
+    <Suspense fallback={<div>로딩중</div>}>
+      <Router basename={'/perf-basecamp'}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,7 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: false
+    minimize: true,
+    usedExports: true
   }
 };
