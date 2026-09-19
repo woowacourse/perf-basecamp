d56a97749983178bce80b325269f2f090d9455b3
janghw0126 <janghw232625@gmail.com>
2026-09-19 09:55:16 +0900

    fix: 타입스크립트 JSX 반환 타입 추가 및 스타일 모듈 선언 수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer/Footer';
 
 import './App.css';
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <Router basename={'/perf-basecamp'}>
       <NavBar />
