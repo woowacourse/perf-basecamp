5130ed928b8875701a3f616ccd8a9a42f1245310
hjkim0905 <eax9952@gmail.com>
2026-09-19 18:05:07 +0900

    feat: apply lazy loading, suspense to divide bundle.js into two chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
 import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+
+const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = (): React.JSX.Element => {
   return (
@@ -14,7 +16,14 @@ const App = (): React.JSX.Element => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search"
+          element={
+            <Suspense fallback={null}>
+              <Search />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
