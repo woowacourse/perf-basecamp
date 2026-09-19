a77e55401d82664051f64a00c3ffc7ccc28eb2b6
boyeon <boyeon6331@gmail.com>
2026-09-20 02:14:32 +0900

    perf: 검색 결과 GIF를 중간 크기 리소스로 요청

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -17,7 +17,7 @@ const convertResponseToModel = (gifList: IGif[]): GifImageModel[] => {
     return {
       id,
       title: title ?? '',
-      imageUrl: images.original.url
+      imageUrl: images.downsized_medium.url
     };
   });
 };
