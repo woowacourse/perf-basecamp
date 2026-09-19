7e42f7e8f78c1f26711b0dd1fc6eac3ea9c8551a
stardusty-lab <startdusty.lab@gmail.com>
2026-09-20 01:21:08 +0900

    refactor(cached): 시간관련 변수명 renaming

--- src/repositoris/gifRepository.ts
+++ src/repositoris/gifRepository.ts
@@ -2,22 +2,22 @@ import { gifAPIService } from '../apis/gifAPIService';
 
 import { GifImageModel } from '../models/image/gifImage';
 
-const cacheTimePolicy = 24 * 60 * 60 * 1000;
+const staleTime = 24 * 60 * 60 * 1000;
 
 let trendingCacheData: {
   data: GifImageModel[];
-  time: number;
+  cachedAt: number;
 } | null = null;
 
 export const gifRepository = {
   getTrending: async (): Promise<GifImageModel[]> => {
-    if (trendingCacheData !== null && trendingCacheData.time + cacheTimePolicy > Date.now())
+    if (trendingCacheData !== null && trendingCacheData.cachedAt + staleTime > Date.now())
       return trendingCacheData.data;
 
     const data = await gifAPIService.getTrending();
     trendingCacheData = {
       data,
-      time: Date.now()
+      cachedAt: Date.now()
     };
     return trendingCacheData.data;
   }
