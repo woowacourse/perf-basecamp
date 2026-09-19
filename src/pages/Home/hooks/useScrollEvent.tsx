7e1cf184257a4baa5bab32e564af57ac1c3cfa98
kojesung <js95112345@gmail.com>
2026-09-20 01:29:54 +0900

    feat: useScrollEvent 호출 시마다 이벤트 리스너 등록 및 해제 반복 방지

--- src/pages/Home/hooks/useScrollEvent.tsx
+++ src/pages/Home/hooks/useScrollEvent.tsx
@@ -1,19 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type ScrollHandler = () => void;
 
 const useScrollEvent = (onScroll: ScrollHandler) => {
+  const savedHandler = useRef(onScroll);
+
   useEffect(() => {
-    const handleScroll = (event: Event) => {
-      onScroll();
-    };
+    savedHandler.current = onScroll;
+  }, [onScroll]);
+
+  useEffect(() => {
+    const handleScroll = () => savedHandler.current();
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [onScroll]);
+  }, []);
 };
 
 export default useScrollEvent;
