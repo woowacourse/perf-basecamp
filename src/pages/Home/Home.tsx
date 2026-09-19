7c9b7cdbe4d83a9be86d04941df8d19580261dfe
janghw0126 <janghw232625@gmail.com>
2026-09-19 11:40:31 +0900

    feat: hero 이미지 WebP 변환 및 해상도 축소

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
+import heroImage from '../../assets/images/hero.webp';
 import trendingGif from '../../assets/images/trending.gif';
 import findGif from '../../assets/images/find.gif';
 import freeGif from '../../assets/images/free.gif';
