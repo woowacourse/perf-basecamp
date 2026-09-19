eba69e0cbe87fb8523cb59c1987fb6aed5db0c95
Antoliny0919 <antoliny0919@gmail.com>
2026-09-19 16:34:06 +0900

    perf: 지연 로딩을 사용하여 페이지별로 청크 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+
+const Home = React.lazy(() => import('./pages/Home/Home'));
+const Search = React.lazy(() => import('./pages/Search/Search'));
 
 import './App.css';
 
 const App = () => {
   return (
-    <Router basename={'/perf-basecamp'}>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Suspense fallback={<p>페이지를 불러오는 중…</p>}>
+      <Router basename={'/perf-basecamp'}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
