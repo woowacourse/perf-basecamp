d7a8d57eb56108d3f3ece5af34a762635d9db531
stardusty-lab <startdusty.lab@gmail.com>
2026-09-20 17:08:47 +0900

    feat(CustomCursor): mouse 좌표 정보 callback으로 받아서 처리

--- src/pages/Home/components/CustomCursor/CustomCursor.tsx
+++ src/pages/Home/components/CustomCursor/CustomCursor.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import useMousePosition from '../../hooks/useMousePosition';
 
 import styles from './CustomCursor.module.css';
 
-type CustomCursorProps = {
+interface CustomCursorProps {
   text: string;
-};
+}
 
 const CustomCursor = ({ text = '' }: CustomCursorProps) => {
   const [...cursorTextChars] = text;
-  const mousePosition = useMousePosition();
-  const cursorRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const updateMousePosition = useCallback(() => {
     if (cursorRef.current != null) {
       cursorRef.current.style.transform = `translate(${mousePosition.pageX}px, ${mousePosition.pageY}px)`;
     }
-  }, [mousePosition]);
+  }, []);
+  const mousePosition = useMousePosition({ callback: updateMousePosition });
+  const cursorRef = useRef<HTMLDivElement>(null);
 
   return (
     <div ref={cursorRef} className={styles.cursor}>
