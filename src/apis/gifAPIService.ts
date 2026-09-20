f825df45ad1aa1881336c9b8e588ea5b1b437af5
lee-eojin <lejin0906@snu.ac.kr>
2026-09-21 00:37:08 +0900

    feat: 검색 결과 카드에 원본 대신 200px WebP 렌디션 사용

--- src/apis/gifAPIService.ts
+++ src/apis/gifAPIService.ts
@@ -17,7 +17,7 @@ const convertResponseToModel = (gifList: IGif[]): GifImageModel[] => {
     return {
       id,
       title: title ?? '',
-      imageUrl: images.original.url
+      imageUrl: images.fixed_width.webp
     };
   });
 };
