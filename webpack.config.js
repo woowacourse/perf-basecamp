c3c0e0ef51ae32cc680e69f0a442f37e2cf0a049
bel1c10ud <mail@bellclou.de>
2026-09-20 19:14:38 +0900

    chore: 프로덕션 빌드에서 CSS 파일을 별도로 출력하도록 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/index.tsx',
@@ -24,7 +27,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    ...(isProduction ? [new MiniCssExtractPlugin()] : [])
   ],
   module: {
     rules: [
@@ -37,7 +41,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
