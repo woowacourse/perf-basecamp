ec2a7d87eacfe519878fc9dd758af18aa641cac5
binggwa <qudrhks317@gmail.com>
2026-09-20 15:26:30 +0900

    perf(home): 초기 화면 청크 요청 단계 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 
 import './App.css';
-
-const Home = React.lazy(
-  () =>
-    import(
-      /* webpackChunkName: "home" */
-      './pages/Home/Home'
-    )
-);
 
 const Search = React.lazy(
   () =>
