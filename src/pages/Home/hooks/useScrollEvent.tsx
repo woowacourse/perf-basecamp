1dd53551c84f3252dc0753d75f6c84d36ce04ffd
stardusty-lab <startdusty.lab@gmail.com>
2026-09-20 18:10:25 +0900

    feat(useScrollEvent): raf를 도입해서 브라우저 프레임 변경에 맞춰 callback 실행

--- src/pages/Home/hooks/useScrollEvent.tsx
+++ src/pages/Home/hooks/useScrollEvent.tsx
@@ -1,17 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type ScrollHandler = () => void;
 
 const useScrollEvent = (onScroll: ScrollHandler) => {
+  const frameRef = useRef<number | null>(null);
   useEffect(() => {
     const handleScroll = (event: Event) => {
-      onScroll();
+      if (frameRef.current !== null) return;
+
+      frameRef.current = requestAnimationFrame(() => {
+        frameRef.current = null;
+        onScroll();
+      });
     };
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      if (frameRef.current !== null) {
+        cancelAnimationFrame(frameRef.current);
+      }
     };
   }, [onScroll]);
 };
