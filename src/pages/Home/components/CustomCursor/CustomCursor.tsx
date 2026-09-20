a6a2eadbebe410240caaebf6d4b396526ed6f0a0
bigcloud07 <dnsxo0712@gmail.com>
2026-09-21 00:28:32 +0900

    feat: 커스텀 커서의 리렌더링 제거와 포인터 환경 대응

--- src/pages/Home/components/CustomCursor/CustomCursor.tsx
+++ src/pages/Home/components/CustomCursor/CustomCursor.tsx
@@ -1,26 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import useMousePosition from '../../hooks/useMousePosition';
 
 import styles from './CustomCursor.module.css';
 
-type CustomCursorProps = {
+interface CustomCursorProps {
   text: string;
-};
+}
 
-const CustomCursor = ({ text = '' }: CustomCursorProps) => {
+const CustomCursor = ({ text = '' }: CustomCursorProps): JSX.Element => {
   const [...cursorTextChars] = text;
-  const mousePosition = useMousePosition();
   const cursorRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (cursorRef.current) {
-      cursorRef.current.style.top = `${mousePosition.pageY}px`;
-      cursorRef.current.style.left = `${mousePosition.pageX}px`;
-    }
-  }, [mousePosition]);
+  useMousePosition(cursorRef);
 
   return (
-    <div ref={cursorRef} className={styles.cursor}>
+    <div ref={cursorRef} className={styles.cursor} aria-hidden="true">
       {cursorTextChars.map((char, index) => (
         <span key={index} className={styles.character}>
           {char}
