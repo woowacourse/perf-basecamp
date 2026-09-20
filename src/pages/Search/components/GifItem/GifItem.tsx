585ea2e1eae57ac65cc9de990fe1ca1792aa160b
boyeon <boyeon6331@gmail.com>
2026-09-20 22:16:15 +0900

    perf: load more 시 기존 GIF 재렌더링 방지

--- src/pages/Search/components/GifItem/GifItem.tsx
+++ src/pages/Search/components/GifItem/GifItem.tsx
@@ -1,10 +1,12 @@
+import { memo } from 'react';
+
 import { GifImageModel } from '../../../../models/image/gifImage';
 
 import styles from './GifItem.module.css';
 
 type GifItemProps = Omit<GifImageModel, 'id'>;
 
-const GifItem = ({ imageUrl = '', title = '' }: GifItemProps) => {
+const GifItem = ({ imageUrl = '', title = '' }: GifItemProps): JSX.Element => {
   return (
     <div className={styles.gifItem}>
       <img className={styles.gifImage} src={imageUrl} />
@@ -16,4 +18,4 @@ const GifItem = ({ imageUrl = '', title = '' }: GifItemProps) => {
   );
 };
 
-export default GifItem;
+export default memo(GifItem);
