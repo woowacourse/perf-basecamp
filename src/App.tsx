f26a3a61f1bcc1d8fef237fdd957feeec3f022d6
jebiyeon02 <jhje5595@naver.com>
2026-09-20 18:30:50 +0900

    refactor: Home 컴포넌트 lazy loading제거
    서비스 진입 시 처음부터 필요한데 굳이 lazy loading으로 지연시키지 않는게 좋다고 판단했습니다. 추가로 home의 lazy loading이 오히려 최초 렌더링 병목이 되는 것도 확인되어 취소했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
-const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
