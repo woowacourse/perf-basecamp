067e13a99bdfea2e78643f6868dccd12f8aaf6eb
bigcloud07 <dnsxo0712@gmail.com>
2026-09-21 00:28:17 +0900

    feat: WebP와 MP4 미디어 모듈 타입 선언

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,4 +1,15 @@
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.gif';
-declare module '*.svg';
+declare module '*.svg' {
+  const url: string;
+  export default url;
+}
+declare module '*.webp' {
+  const url: string;
+  export default url;
+}
+declare module '*.mp4' {
+  const url: string;
+  export default url;
+}
