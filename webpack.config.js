d1ef60a6c146d1bd0817cd807d6fa7ab81e09855
김예안 <kjan0929@naver.com>
2026-09-20 21:52:09 +0900

    chore: search 페이지 코드 스플리팅 적용

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
   output: {
     filename: 'bundle.js',
+    chunkFilename: '[name].[contenthash:8].chunk.js',
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
     clean: true
