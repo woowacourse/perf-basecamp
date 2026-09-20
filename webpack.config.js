fff65dd7b69e38805b5d68b971ea0268fec4a42d
janghw0126 <janghw232625@gmail.com>
2026-09-20 13:06:19 +0900

    chore: webpack-bundle-analyzer 도입

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
@@ -28,7 +30,18 @@ module.exports = {
       patterns: [{ from: './public', to: './public' }]
     }),
     new Dotenv(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    ...(process.env.ANALYZE === 'true'
+      ? [
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: '../report/bundle-report.html',
+            openAnalyzer: false,
+            generateStatsFile: true,
+            statsFilename: '../report/stats.json'
+          })
+        ]
+      : [])
   ],
   module: {
     rules: [
