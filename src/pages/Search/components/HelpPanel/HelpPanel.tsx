3f4dee44db1ee1ee3a9be8ea1174139943da85e9
iftype <conditionaltype@gmail.com>
2026-09-21 15:37:32 +0900

    perf: 도움말 패널 렌더링 최적화

--- src/pages/Search/components/HelpPanel/HelpPanel.tsx
+++ src/pages/Search/components/HelpPanel/HelpPanel.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { memo, useState } from 'react';
 import { AiOutlineInfo, AiOutlineClose } from 'react-icons/ai';
 import classNames from 'classnames/bind';
 
@@ -37,35 +37,39 @@ const HelpPanel = () => {
           </button>
         </div>
         <div className={styles.sheetContentsContainer}>
-          <img
-            src="https://media0.giphy.com/media/3oKIPdiPGxPI7Dze7u/giphy.gif?cid=ecf05e475f5bct6ci09g3pgn43nf6bausx33fj7f96f6ig92&rid=giphy.gif&ct=g"
-            alt=""
-          />
-          <p>
-            'memegle' is powered by GIPHY, the top source for the best & newest GIFs & Animated
-            Stickers online. You can find any gif uploaded on GIPHY here.
-          </p>
-          <br />
+          {isShow && (
+            <>
+              <img
+                src="https://media0.giphy.com/media/3oKIPdiPGxPI7Dze7u/giphy.gif?cid=ecf05e475f5bct6ci09g3pgn43nf6bausx33fj7f96f6ig92&rid=giphy.gif&ct=g"
+                alt=""
+              />
+              <p>
+                'memegle' is powered by GIPHY, the top source for the best & newest GIFs & Animated
+                Stickers online. You can find any gif uploaded on GIPHY here.
+              </p>
+              <br />
 
-          <img src="https://giphy.com/static/img/artistdirectory_1040.gif" alt="gif image" />
-          <p>
-            If you want more, you are always welcome to contribute as an artist. Please refer to the
-            guideline&nbsp;
-            <a href="https://support.giphy.com/hc/en-us/articles/360019977552-How-to-Upload">
-              here
-            </a>
-            &nbsp;and upload your work!
-          </p>
-          <br />
-          <p>Here are some artists you can refer to.</p>
-          <br />
-          <section>
-            <ArtistList artists={artists} />
-          </section>
+              <img src="https://giphy.com/static/img/artistdirectory_1040.gif" alt="gif image" />
+              <p>
+                If you want more, you are always welcome to contribute as an artist. Please refer to
+                the guideline&nbsp;
+                <a href="https://support.giphy.com/hc/en-us/articles/360019977552-How-to-Upload">
+                  here
+                </a>
+                &nbsp;and upload your work!
+              </p>
+              <br />
+              <p>Here are some artists you can refer to.</p>
+              <br />
+              <section>
+                <ArtistList artists={artists} />
+              </section>
+            </>
+          )}
         </div>
       </section>
     </>
   );
 };
 
-export default HelpPanel;
+export default memo(HelpPanel);
