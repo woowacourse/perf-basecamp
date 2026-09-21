9e61206955089844db92dd6afc4d848dd6a85bf3
iftype <conditionaltype@gmail.com>
2026-09-21 15:40:33 +0900

    fix: 푸터 링크 색상 대비 개선

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
-import './Footer.module.css';
+import styles from './Footer.module.css';
 
-const Footer = () => {
+const Footer = (): JSX.Element => {
   return (
-    <footer>
-      <p>
+    <footer className={styles.footer}>
+      <p className={styles.text}>
         (c) memegle. All rights reserved. Powered by&nbsp;
-        <a href="https://giphy.com/">Giphy</a>.
+        <a className={styles.link} href="https://giphy.com/">
+          Giphy
+        </a>
+        .
       </p>
     </footer>
   );
