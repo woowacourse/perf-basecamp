c940b8ffc003cdf681e374981d8d24c036b3eadc
gustn99 <azerosong@gmail.com>
2026-09-22 23:55:17 +0900

    fix: font-face에서 해시가 붙은 폰트를 제대로 로드하도록 수정

--- scripts/preload-fonts-webpack-plugin.js
+++ scripts/preload-fonts-webpack-plugin.js
@@ -1,7 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PLUGIN_NAME = 'PreloadFontsWebpackPlugin';
-const FONT_STYLES = ['normal', 'italic'];
+
+const FONT_FACES = [
+  {
+    style: 'normal',
+    assetPattern: /^static\/josefin-sans-normal\.[^.]+\.woff2$/
+  },
+  {
+    style: 'italic',
+    assetPattern: /^static\/josefin-sans-italic\.[^.]+\.woff2$/
+  }
+];
+
+const UNICODE_RANGE =
+  'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, ' +
+  'U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, ' +
+  'U+2212, U+2215, U+FEFF, U+FFFD';
 
 class PreloadFontsWebpackPlugin {
   apply(compiler) {
@@ -10,27 +25,50 @@ class PreloadFontsWebpackPlugin {
         PLUGIN_NAME,
         (data) => {
           const assetNames = compilation.getAssets().map(({ name }) => name);
-          const fontAssets = FONT_STYLES.map((style) =>
-            assetNames.find((name) =>
-              new RegExp(`^static/josefin-sans-${style}\\.[^.]+\\.woff2$`).test(name)
-            )
-          );
+          const resolved = FONT_FACES.map(({ style, assetPattern }) => {
+            const name = assetNames.find((n) => assetPattern.test(n));
 
-          if (fontAssets.some((name) => name == null)) {
-            compilation.errors.push(
-              new Error(`${PLUGIN_NAME}: Could not find all emitted font assets.`)
-            );
+            if (!name) {
+              compilation.errors.push(
+                new Error(
+                  `${PLUGIN_NAME}: Could not find emitted font asset for style "${style}".`
+                )
+              );
+            }
+
+            return { style, name };
+          });
+
+          if (resolved.some(({ name }) => name == null)) {
             return data;
           }
 
-          const preloadLinks = fontAssets
+          const preloadLinks = resolved
             .map(
-              (name) =>
-                `<link rel="preload" href="./${name}" as="font" type="font/woff2" crossorigin />`
+              ({ name }) =>
+                `<link rel="preload" href="${name}" as="font" type="font/woff2" crossorigin />`
             )
             .join('\n  ');
 
-          data.html = data.html.replace('</head>', `  ${preloadLinks}\n</head>`);
+          const fontFaceRules = resolved
+            .map(
+              ({ style, name }) =>
+                `@font-face {
+      font-family: 'Josefin Sans';
+      font-style: ${style};
+      font-weight: 400 700;
+      font-display: swap;
+      src: url('${name}') format('woff2');
+      unicode-range: ${UNICODE_RANGE};
+    }`
+            )
+            .join('\n    ');
+
+          const injection = `  ${preloadLinks}
+  <style>${fontFaceRules}</style>
+`;
+
+          data.html = data.html.replace('</head>', `${injection}</head>`);
 
           return data;
         }
--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ const Search = React.lazy(() => import('./pages/Search/Search'));
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
+// @font-face는 PreloadFontsWebpackPlugin이 해시된 경로로 HTML에 주입합니다.
+// 아래 import는 webpack이 폰트 파일을 emit하도록 트리거합니다.
+import './assets/fonts/josefin-sans-normal.woff2';
+import './assets/fonts/josefin-sans-italic.woff2';
+
 import './App.css';
 
 const App = () => {
