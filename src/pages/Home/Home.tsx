584dea37939bc6753be859a1be5551b730aa429a
TH_97 <hood3392@gmail.com>
2026-09-22 22:50:08 +0900

    feat: 요청 크기 최적화

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,10 @@ import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
-import heroImage from '../../assets/images/hero.png';
-import trendingGif from '../../assets/images/trending.gif';
-import findGif from '../../assets/images/find.gif';
-import freeGif from '../../assets/images/free.gif';
+import heroImage from '../../assets/images/hero.png?as=hero-webp';
+import trendingImage from '../../assets/images/trending.gif?as=animated-webp';
+import findImage from '../../assets/images/find.gif?as=animated-webp';
+import freeImage from '../../assets/images/free.gif?as=animated-webp';
 
 import FeatureItem from './components/FeatureItem/FeatureItem';
 import CustomCursor from './components/CustomCursor/CustomCursor';
@@ -21,7 +21,14 @@ const Home = () => {
   return (
     <>
       <section className={styles.heroSection}>
-        <img className={styles.heroImage} src={heroImage} alt="hero image" />
+        <img
+          className={styles.heroImage}
+          src={heroImage}
+          alt=""
+          width="1600"
+          height="1067"
+          fetchPriority="high"
+        />
         <div className={styles.projectTitle}>
           <h1 className={styles.title}>Memegle</h1>
           <h3 className={styles.subtitle}>gif search engine for you</h3>
@@ -35,9 +42,9 @@ const Home = () => {
         <div className={styles.featureSectionWrapper}>
           <h2 className={styles.featureTitle}>Features</h2>
           <div className={styles.featureItemContainer}>
-            <FeatureItem title="See trending gif" imageSrc={trendingGif} />
-            <FeatureItem title="Find gif for free" imageSrc={findGif} />
-            <FeatureItem title="Free for everyone" imageSrc={freeGif} />
+            <FeatureItem title="See trending gif" imageSrc={trendingImage} />
+            <FeatureItem title="Find gif for free" imageSrc={findImage} />
+            <FeatureItem title="Free for everyone" imageSrc={freeImage} />
           </div>
           <Link to="/search">
             <button className={styles.linkButton}>start search</button>
