911acb6633a2fede4df3a1529b03175effcb1002
eunnn <ehddms0517@naver.com>
2026-09-23 12:16:19 +0900

    fix: MiniCssExtractPlugin의 출력 경로를 static/css로 수정

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,8 @@ module.exports = {
     ...(isProduction
       ? [
           new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash].css'
+            filename: 'static/css/[name].[contenthash].css',
+            chunkFilename: 'static/css/[id].[contenthash].css'
           })
         ]
       : []),
