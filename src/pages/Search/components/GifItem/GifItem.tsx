500de124607e145d5ff6c2e8c25a7879eef59470
binggwa <qudrhks317@gmail.com>
2026-09-23 11:48:00 +0900

    perf(search): 화면 밖 GIF 이미지 로딩 지연

--- src/pages/Search/components/GifItem/GifItem.tsx
+++ src/pages/Search/components/GifItem/GifItem.tsx
@@ -3,12 +3,14 @@ import { memo } from 'react';
 
 import styles from './GifItem.module.css';
 
-type GifItemProps = Omit<GifImageModel, 'id'>;
+type GifItemProps = Omit<GifImageModel, 'id'> & {
+  loading?: 'eager' | 'lazy';
+};
 
-const GifItem = ({ imageUrl = '', title = '' }: GifItemProps) => {
+const GifItem = ({ imageUrl = '', title = '', loading = 'lazy' }: GifItemProps) => {
   return (
     <div className={styles.gifItem}>
-      <img className={styles.gifImage} src={imageUrl} />
+      <img className={styles.gifImage} src={imageUrl} loading={loading} />
       <div className={styles.gifTitleContainer}>
         <div className={styles.gifTitleBg}></div>
         <h4 className={styles.gifTitle}>{title}</h4>
